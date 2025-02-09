23e912302677406108a70de5a50cce4e211ffb12
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-02-09 08:13:37 -0800

    add aiken env

---
+++ milestone2/aiken/README.md
@@ -0,0 +1,9 @@
+# aiken env
+
+Just Aiken, without MeshJS or PyCardano.
+
+```bash
+$ nix develop
+$ aiken --version
+aiken v1.1.10+b25afa2
+```
---
+++ milestone2/aiken/flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Aiken test env";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    aiken.url = "github:aiken-lang/aiken/v1.1.10";
+  };
+  outputs = { self, nixpkgs, aiken, ... }: {
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    devShells.x86_64-linux.default = self.pkgs.mkShell {
+      buildInputs = [
+        aiken.packages.x86_64-linux.aiken
+      ];
+    };
+
+  };
+}
