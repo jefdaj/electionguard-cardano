749bc681f73016aa8f12e8643ce97e32464e11e5
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-02-16 11:31:34 -0800

    multiscript_election draft build_manifest step

---
+++ milestone1/multiscript_election/admin.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python3
+
+# Based on the functional_key_ceremony integration test.
+# Instead of one script, this has one script per party
+# and they coordinate via a shared folder on the local filesystem.
+# This script is written from the election admin's point of view.
+
+import click
+import json
+from pprint import pprint
+from os import makedirs
+from os.path import join
+from typing import List
+from datetime import datetime, timedelta
+
+from electionguard.key_ceremony import (
+    combine_election_public_keys,
+    ElectionPublicKey,
+)
+from electionguard import serialize
+
+from guardian import load_guardian_pubkeys
+
+@click.command("build-manifest")
+@click.option(
+    "--public-records-dir",
+    prompt="Public records directory",
+    help="The location of a directory into which will be placed all public records. "
+    + "This folder should be protected. Existing files will be overwritten.",
+    type=click.Path(exists=False, dir_okay=True, file_okay=False, resolve_path=True),
+)
+@click.option(
+    "--referendum-question",
+    prompt="Referendum style yes-or-no question",
+    help="The yes-or-no question to be put to voters in this test election.",
+    type=click.STRING,
+)
+def BuildManifestCommand(
+    public_records_dir: str,
+    referendum_question: str
+) -> None:
+    """Build a minimal valid manifest.
+    For now it handles only one referendum-style yes or no question,
+    provided by the user.
+    """
+    print(json.dumps(locals()))
+
+    now = datetime.utcnow()
+    county_id = "electionguard-cardano-test-county"
+    contest_name = referendum_question
+
+    manifest = {
+        'election_scope_id': 'electionguard-cardano-test-manifest',
+        'spec_version': '1.0', # TODO is this right?
+        'type': 'general',
+        'start_date': now,
+        'end_date': now + timedelta(days=2, hours=12), # TODO does it matter?
+        'geopolitical_units': [{
+            "object_id": county_id,
+            "name": "ElectionGuard + Cardano County",
+            "type": "municipality",
+            "contact_information": None,
+        }],
+        "parties": [{
+            "object_id": "N/A",
+            "name": {
+                "text": []
+            },
+            "abbreviation": None,
+            "color": None,
+            "logo_uri": None
+        }],
+        "candidates": [
+            {
+                "object_id": "referendum-question-affirmative",
+                "name": {
+                    "text": []
+                },
+                "party_id": None,
+                "image_uri": None,
+                "is_write_in": None
+            },
+            {
+                "object_id": "referendum-question-negative",
+                "name": {
+                    "text": []
+                },
+                "party_id": None,
+                "image_uri": None,
+                "is_write_in": None
+            }
+        ],
+        "contests": [{
+            "object_id": "referendum-question",
+            "sequence_order": 0,
+            "electoral_district_id": county_id,
+            "vote_variation": "one_of_m",
+            "number_elected": 1,
+            "votes_allowed": 1,
+            "name": contest_name,
+            "ballot_selections": [
+                {
+                    "object_id": "referendum-question-affirmative-selection",
+                    "sequence_order": 0,
+                    "candidate_id": "referendum-question-affirmative"
+                },
+                {
+                    "object_id": "referendum-question-negative-selection",
+                    "sequence_order": 1,
+                    "candidate_id": "referendum-question-negative"
+                }
+            ],
+            "ballot_title": None,
+            "ballot_subtitle": None
+        }],
+        "ballot_styles": [{
+            "object_id": "ballot-style-01",
+            "geopolitical_unit_ids": [county_id],
+            "party_ids": None,
+            "image_uri": None
+        }],
+        "name": {
+            "text": [
+                {
+                    "value": "ElectionGuard + Cardano Test Election",
+                    "language": "en"
+                },
+                {
+                    # TODO spanish for test?
+                    "value": "Eleccion del ElectionGuard + Cardano",
+                    "language": "es"
+                }
+            ]
+        },
+        "contact_information": None
+    }
+
+    manifest_name = '1_manifest'
+    serialize.to_file(manifest, manifest_name, public_records_dir)
+
+
+@click.command("announce-key-ceremony")
+@click.option(
+    "--public-records-dir",
+    prompt="Public records directory",
+    help="The location of a directory into which will be placed all public records. "
+    + "This folder should be protected. Existing files will be overwritten.",
+    type=click.Path(exists=False, dir_okay=True, file_okay=False, resolve_path=True),
+)
+@click.option(
+    "--guardian-count",
+    prompt="Number of s",
+    help="The number of guardians that will participate in the key ceremony and tally.",
+    type=click.INT,
+)
+@click.option(
+    "--quorum",
+    prompt="Quorum",
+    help="The minimum number of guardians required to show up to the tally.",
+    type=click.INT,
+)
+def AnnounceKeyCeremonyCommand(
+    guardian_count: int,
+    quorum: int,
+    public_records_dir: str,
+) -> None:
+    """Announce key ceremony parameters.
+    This is a provisional thing based on the electionguard_gui key_ceremony_service.py;
+    I think eventually what we want is for everything to flow from the manifest instead.
+    """
+    print(json.dumps(locals()))
+
+    ceremony_dir = join(public_records_dir, '2_key_ceremony')
+    makedirs(ceremony_dir, exist_ok=True)
+
+    # based on electionguard-python/src/electionguard_gui/models/key_ceremony_service:create
+    announcement = {
+        "created_at": datetime.utcnow(),
+        "guardian_count": guardian_count,
+        "quorum": quorum,
+        # "backups": [],
+        # "completed_at": None,
+        # "created_by": self._auth_service.get_user_id(),
+        # "guardians_joined": [],
+        # "guardians_keys": [],
+        # "joint_key": None,
+        # "key_ceremony_name": key_ceremony_name,
+        # "keys": [],
+        # "other_keys": [],
+        # "shared_backups": [],
+        # "verifications": [],
+    }
+    announcement_name = '0_announce'
+    serialize.to_file(announcement, announcement_name, ceremony_dir)
+
+
+@click.command("publish-joint-key")
+@click.option(
+    "--public-records-dir",
+    prompt="Public records directory",
+    help="The location of a directory into which will be placed the guardian's public keys "
+    + "This folder should be protected. Existing files will be overwritten.",
+    type=click.Path(exists=False, dir_okay=True, file_okay=False, resolve_path=True),
+)
+def PublishJointKeyCommand(
+    public_records_dir: str,
+) -> None:
+    """Final step in the key ceremony.
+    Could technically be posted on chain by anyone, not just the admin.
+    """
+    print(json.dumps(locals()))
+
+    ceremony_dir = join(public_records_dir, '2_key_ceremony')
+    pubkeys_dir = join(ceremony_dir, '1_pubkeys')
+    makedirs(pubkeys_dir, exist_ok=True)
+
+    guardian_public_keys: List[ElectionPublicKey] = load_guardian_pubkeys(pubkeys_dir)
+
+    election_joint_key = combine_election_public_keys(guardian_public_keys)
+    assert election_joint_key is not None
+
+    # NOTE we skip 4 to leave room for the challenge step
+    joint_key_name = '5_joint_key'
+    serialize.to_file(election_joint_key, joint_key_name, ceremony_dir)
+
+
+@click.group()
+def cli() -> None:
+    pass
+
+cli.add_command(BuildManifestCommand)
+cli.add_command(AnnounceKeyCeremonyCommand)
+cli.add_command(PublishJointKeyCommand)
+
+if __name__ == '__main__':
+    cli()
---
+++ milestone1/multiscript_election/main.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+# This is the main orchestration script. For now it will expect to run from
+# /repo inside the electionguard-python-makefile-docker-env container.
+
+import subprocess
+import json
+
+from os import makedirs
+from os.path import join
+from pprint import pprint
+
+
+MSKC_NUMBER_OF_GUARDIANS = 3
+MSKC_QUORUM = 2
+MSKC_ROOT = '/repo/multiscript_election'
+MSKC_SRC = MSKC_ROOT
+
+GUARDIAN_SEQUENCE_ORDERS = [*range(1, MSKC_NUMBER_OF_GUARDIANS + 1)]
+GUARDIAN_IDS = [f"guardian_{i}" for i in GUARDIAN_SEQUENCE_ORDERS]
+
+PUBLIC_RECORDS_DIR  = join(MSKC_ROOT, 'public_record')
+PRIVATE_RECORDS_DIR = join(MSKC_ROOT, 'private_records')
+
+
+def run_python_script(args, **kwargs):
+    # TODO docker exec inside each container here?
+    args = ["poetry", "run"] + args
+    kwargs.update(stdout=subprocess.PIPE, text=True)
+    proc = subprocess.Popen(args, **kwargs)
+    (stdout, stderr) = proc.communicate()
+    try:
+        pprint(json.loads(stdout))
+    except json.decoder.JSONDecodeError:
+        print(stdout)
+        print(stderr)
+
+
+def build_manifest():
+    # uncomment for interactive script:
+    # question = input('Referendum-style question to be asked: ')
+    question = 'Are pineapples still cool?'
+    run_python_script([
+        join(MSKC_SRC, 'admin.py'), "build-manifest",
+        "--public-records-dir", PUBLIC_RECORDS_DIR,
+        "--referendum-question", question,
+    ], stdin=subprocess.PIPE)
+
+
+def announce_key_ceremony():
+    run_python_script([
+        join(MSKC_SRC, 'admin.py'), "announce-key-ceremony",
+        "--guardian-count"    , str(MSKC_NUMBER_OF_GUARDIANS),
+         "--quorum"           , str(MSKC_QUORUM),
+        "--public-records-dir", PUBLIC_RECORDS_DIR,
+    ])
+
+
+def publish_joint_key():
+    run_python_script([
+        join(MSKC_SRC, 'admin.py'), "publish-joint-key",
+        "--public-records-dir", PUBLIC_RECORDS_DIR,
+    ])
+
+
+def context():
+    pass
+    # TODO write this
+
+
+def key_ceremony_round(current_round):
+    for guardian_id, sequence_order in zip(GUARDIAN_IDS, GUARDIAN_SEQUENCE_ORDERS):
+        run_python_script([
+            join(MSKC_SRC, 'guardian.py'), "key-ceremony",
+            "--guardian-count"         , str(MSKC_NUMBER_OF_GUARDIANS),
+            "--quorum"                 , str(MSKC_QUORUM),
+            "--public-records-dir"     , PUBLIC_RECORDS_DIR,
+            "--private-records-dir"    , PRIVATE_RECORDS_DIR,
+            "--guardian-id"            , guardian_id,
+            "--guardian-sequence-order", str(sequence_order),
+            "--current-round"          , str(current_round),
+        ])
+
+
+if __name__ == '__main__':
+    build_manifest()
+    announce_key_ceremony()
+    key_ceremony_round(1)
+    key_ceremony_round(2)
+    key_ceremony_round(3)
+    # TODO should there be a "publish final guardian records" step here?
+    publish_joint_key()
+    # context()
