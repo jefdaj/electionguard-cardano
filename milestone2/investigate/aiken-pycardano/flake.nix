71df3a6c9dec6d57c9d129a42ce13ce10d4ca07b
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-02-18 06:58:49 -0800

    start aiken-pycardano

--- milestone2/investigate/aiken-pycardano/flake.nix
+++ milestone2/investigate/aiken-pycardano/flake.nix
@@ -1,60 +1,19 @@
 {
-  description = "Aiken + PyCardano env";
-
-  # based on https://www.zknotes.com/page/python%20development%20flake
-
-  # to activate, type `nix develop` while in the repo dir or a subdir.
-  # or use direnv to automatically do so (see .envrc)
-  # for best results, don't have a global python installed.  mixing python
-  # versions can make for venv problems.
-
+  description = "Aiken + MeshJS test env";
   inputs = {
-    nixpkgs.url     = "github:NixOS/nixpkgs/nixos-24.11";
-    aiken.url       = "github:aiken-lang/aiken/v1.1.10";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    aiken.url = "github:aiken-lang/aiken/v1.1.10";
   };
+  outputs = { self, nixpkgs, aiken, ... }: {
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    devShells.x86_64-linux.default = self.pkgs.mkShell {
+      buildInputs = [
+        aiken.packages.x86_64-linux.aiken
+        # self.pkgs.pnpm
+        # self.pkgs.nodePackages.typescript
+        # self.pkgs.nodePackages.typescript-language-server
+      ];
+    };
 
-  outputs = { self, nixpkgs, aiken, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pname = "aiken + python dev environment";
-        pkgs = nixpkgs.legacyPackages."${system}";
-        venvDir = ".venv";
-      in
-        rec {
-          inherit pname;
-
-          # `nix develop`
-          devShell = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              aiken.packages.x86_64-linux.aiken
-              python3Packages.python
-              python3Packages.python-lsp-server
-              python3Packages.autopep8
-            ];
-
-            shellHook = ''
-                # create a virtualenv if there isn't one.
-
-                # DOESN'T install deps for the python app.  Do that once `nix develop` runs with
-                # $ cd src
-                # $ pip install -r ./requirements.txt
-
-                if [ -d "${venvDir}" ]; then
-                  echo "Skipping venv creation, '${venvDir}' already exists"
-                else
-                  echo "Creating new venv environment in path: '${venvDir}'"
-                  # Note that the module venv was only introduced in python 3, so for 2.7
-                  # this needs to be replaced with a call to virtualenv
-                  python -m venv "${venvDir}"
-                  # unescape to attempt use
-                  # \$\{pythonPackages.python.interpreter\} -m venv "${venvDir}"
-                fi
-
-                # activate our virtual env.
-                source "${venvDir}/bin/activate"
-              '';
-          };
-        }
-    );
+  };
 }
