5a8e1f2122c6b86add1646cf305da5d0487dd50c
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-02-26 08:45:22 -0800

    pubsub top-level flake

---
+++ milestone2/pubsub/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Main pubsub flake with Arion";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  outputs = { self, nixpkgs, ... }: {
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    devShells.x86_64-linux.default = self.pkgs.mkShell {
+      buildInputs = with self.pkgs; [
+
+        arion
+        jq
+        time
+        tree
+
+      ];
+    };
+  };
+}
