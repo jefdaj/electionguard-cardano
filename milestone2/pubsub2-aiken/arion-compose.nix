f930bfb03de6db4ccfa5ed5673ae78081a64574f
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-03-03 09:14:33 -0800

    split off publisher/arion.nix

--- milestone2/pubsub2-aiken/arion-compose.nix
+++ milestone2/pubsub2-aiken/arion-compose.nix
@@ -16,48 +16,14 @@ let
   utils = import ./utils.nix;
   inherit (utils) mkNetworks mkStaticIp mkLan mkWan;
 
-  mkNodeConfig  = import ./node/arion.nix;
   mkIpfsService = import ./ipfs.nix { inherit TMP_DATA mkStaticIp mkWan; };
 
-  # roleNumber is appended to the role name: "sub1", "sub2", ...
-  # subnetNumber is the 2nd part of the ip addr like 127.{subnetNumber}.0.3
-  mkPublisherConfig = roleNumber: subnetNumber:
-    let
-      ipfsHost = "172.${toString subnetNumber}.0.2";
-      ipfsAddr = "/ip4/${ipfsHost}/tcp/5001";
-      pubName  = "pub${toString roleNumber}";
-      pubAddr  = "172.${toString subnetNumber}.0.3";
-      pubData  = "${TMP_DATA}/${pubName}-publish";
-    in {
-      networks = mkNetworks pubName subnetNumber;
-      services = {
-        "${pubName}-ipfs" = mkIpfsService pubName subnetNumber;
-        "${pubName}-publish" = {
-          image.enableRecommendedContents = true; # sh, env, misc lightweight files
-          service.useHostStore = true;
-          service.stop_signal = "SIGINT";
-          service.environment.IPFS_API_ADDR = ipfsAddr;
-          image.contents = [
-            flake.packages.x86_64-linux.publisher
-          ];
-          service.volumes = [
-            "${SHARED_CIDS_DIR}:/new_cids"
-            "${pubData}:/upload"
-          ];
-          service.command = [
-            "publish.py"
-            "/upload"
-            "/new_cids/new_cids.txt"
-          ];
-          service.networks = mkStaticIp pubName subnetNumber 3;
-          service.restart = "on-failure";
-          # TODO proper syntax for this?
-          # service.depends = [
-          #   (pubName + "-ipfs")
-          # ];
-        };
-      };
-    };
+  mkNodeConfig = import ./node/arion.nix;
+
+  mkPublisherConfig = import ./publisher/arion.nix {
+    inherit TMP_DATA SHARED_CIDS_DIR mkNetworks mkIpfsService mkStaticIp;
+    publisherPkg = flake.packages.x86_64-linux.publisher;
+  };
 
   # roleNumber is appended to the role name: "sub1", "sub2", ...
   # subnetNumber is the 2nd part of the ip addr like 127.{subnetNumber}.0.2
