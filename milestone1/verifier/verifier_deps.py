4942d7d2e5e19296937369093748a2bceb0850f6
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-03-08 11:23:14 -0800

    start writing v2 of the verifier

--- milestone1/verifier/verifier_deps.py
+++ milestone1/verifier/verifier_deps.py
@@ -5,7 +5,10 @@ import pygraphviz as pgv
 if __name__ == '__main__':
 
     G = pgv.AGraph('verifier_deps.dot')
-    print(G)
 
-    print('dependencies of plaintext_tally:')
-    print(G.predecessors('plaintext_tally'))
+    # stub out verify_* fns in verifier2
+    for node in G.nodes():
+        print(f'def verify_{node}() -> Optional[str]:\n    return "not implemented yet"\n')
+
+    # print('dependencies of plaintext_tally:')
+    # print(G.predecessors('plaintext_tally'))
