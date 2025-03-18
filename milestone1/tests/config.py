f8e21201a985ac91cdc0943e00c8f40618e8822c
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-03-18 10:08:58 -0700

    wip config.py

--- milestone1/tests/config.py
+++ milestone1/tests/config.py
@@ -1,44 +1,3 @@
-from dataclasses import dataclass, field
-from typing import Dict
-import json
-
-# @dataclass(frozen=True)
-# class ArionConfig:
-#     project_name: str = 'test'
-#     bind_mounts: Dict[str, str] = field(default={
-#         "scripts": "/scripts",
-#         "public": "/data/public",
-#         "private": "/data/private"
-#     })
-# 
-# @dataclass(frozen=True)
-# class GuardiansConfig:
-#     count: int
-#     quorum: int
-# 
-# @dataclass(frozen=True)
-# class DevicesConfig:
-#     count: int
-# 
-# @dataclass(frozen=True)
-# class VerifiersConfig:
-#     count: int
-# 
-# @dataclass(frozen=True)
-# class VotesConfig:
-#     votes: Dict[str, Dict[str, int]] = field(default={
-#         "Yes":    {"cast": 3, "spoil":1},
-#         "No":     {"cast": 2, "spoil":2},
-#         "Unsure": {"cast": 1, "spoil":3}
-#     })
-# 
-# @dataclass(frozen=True)
-# class ElectionConfig:
-#     guardians: GuardiansConfig
-#     devices: DevicesConfig
-#     verifiers: VerifiersConfig
-#     question: str = field(default="Are pineapples cool?")
-
 class ArionConfig(dict):
     def __init__(self):
         super(ArionConfig, self).__init__()
@@ -51,7 +10,9 @@ class ArionConfig(dict):
 
 class VoteConfig(dict):
     def __init__(self, n_cast: int, n_spoil: int):
-        self['cast'] = n_cast
+        assert n_cast  >= 0
+        assert n_spoil >= 0
+        self['cast' ] = n_cast
         self['spoil'] = n_spoil
 
 class VotesConfig(dict):
@@ -62,35 +23,39 @@ class VotesConfig(dict):
         self["Unsure"] = VoteConfig(1, 3)
 
 class GuardiansConfig(dict):
-    def __init__(self, count: int, quorum: int):
+    def __init__(self, count: int = 3, quorum: int = 2):
         super(GuardiansConfig, self).__init__()
-        self['count'] = count
+        assert quorum > 0
+        assert quorum <= count
+        self['count' ] = count
         self['quorum'] = quorum
 
 class DevicesConfig(dict):
-    def __init__(self, count: int):
+    def __init__(self, count: int = 4):
         super(DevicesConfig, self).__init__()
+        assert count >= 1
         self['count'] = count
 
 class VerifiersConfig(dict):
-    def __init__(self, count: int):
+    def __init__(self, count: int = 2):
         super(VerifiersConfig, self).__init__()
         self['count'] = count
 
-@dataclass
+class ElectionConfig(dict):
+    def __init__(self,
+        guardians_count  : int = 3,
+        guardians_quorum : int = 2,
+        devices_count    : int = 3,
+        verifiers_count  : int = 2,
+    ):
+        super(ElectionConfig, self).__init__()
+        self['guardians'] : GuardiansConfig(guardians_count, guardians_quorum)
+        self['devices'  ] : DevicesConfig(devices_count)
+        self['verifiers'] : VerifiersConfig(verifiers_count)
+
 class MainConfig(dict):
-
-    guardians_count  : int = field(default=3)
-    guardians_quorum : int = field(default=3)
-    devices_count    : int = field(default=2)
-    verifiers_count  : int = field(default=2)
-
-    def __init__(self, *args, **kwargs):
-        super(MainConfig, self).__init__(*args, **kwargs)
-        self['arion'] = ArionConfig()
-        self['election'] = {
-            'guardians' : GuardiansConfig(self.guardians_count, self.guardians_quorum),
-            'devices'   : DevicesConfig(self.devices_count),
-            'verifiers' : VerifiersConfig(self.verifiers_count),
-        }
-        self['votes'] = VotesConfig()
+    def __init__(self):
+        super(MainConfig, self).__init__()
+        self['arion'   ] = ArionConfig()
+        self['election'] = ElectionConfig()
+        self['votes'   ] = VotesConfig()
