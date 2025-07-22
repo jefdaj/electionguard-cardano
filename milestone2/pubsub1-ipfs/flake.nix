0ac9c50677b73adfcc638f7515d7fe5ae5bb26c1
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-07-22 07:57:26 -0700

    pubsub1-ipfs flake: nixos-24.11 -> 25.05

--- milestone2/pubsub1-ipfs/flake.nix
+++ milestone2/pubsub1-ipfs/flake.nix
@@ -2,7 +2,7 @@
   description = "pubsub dApp test #1 ipfs only";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs = { self, nixpkgs }:
