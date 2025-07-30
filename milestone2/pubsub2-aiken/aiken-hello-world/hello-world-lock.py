c9cfb8bad2cf6913c93633e46a562d1a77832eb2
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-07-30 09:52:54 -0700

    wip pycardano with ogmios

--- milestone2/pubsub2-aiken/aiken-hello-world/hello-world-lock.py
+++ milestone2/pubsub2-aiken/aiken-hello-world/hello-world-lock.py
@@ -4,13 +4,7 @@
 # https://aiken-lang.org/example--hello-world/end-to-end/pycardano
 # https://ogmios-python.readthedocs.io/en/latest/examples/build_tx_pycardano.html
 
-from pycardano import (
-    # BlockFrostChainContext,
-    OgmiosV6ChainContext,
-    PaymentSigningKey,
-    PlutusV3Script,
-    ScriptHash,
-)
+from pycardano import *
 import os
 
 def read_validator() -> dict:
@@ -26,12 +20,10 @@ def read_validator() -> dict:
         "script_hash": script_hash,
     }
 
-# context = BlockFrostChainContext(
-#     project_id=os.environ["BLOCKFROST_PROJECT_ID"],
-#     base_url="https://cardano-preview.blockfrost.io/api/",
-# )
+def main():
+	# TODO thread host and port from top level arion-compose
+	context = OgmiosV6ChainContext("172.13.0.3", 1337)
+	signing_key = PaymentSigningKey.load("keys/me.sk")
 
-# TODO thread host and port from top level arion-compose
-context = OgmiosV6ChainContext("172.13.0.3", 1337)
-
-signing_key = PaymentSigningKey.load("aiken/keys/me.sk")
+if __name__ == '__main__':
+	main()
