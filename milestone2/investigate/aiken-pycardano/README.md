913545858718398b224f3c68c4a43f35121e1657
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-07-30 14:54:26 -0700

    factor aiken-hello-world out of pubsub2-aiken

--- milestone2/investigate/aiken-pycardano/README.md
+++ milestone2/investigate/aiken-pycardano/README.md
@@ -4,11 +4,28 @@ Following the [Aiken + PyCardano hello-world](https://aiken-lang.org/example--he
 
 ```bash
 nix develop
-pip install pycardano
 
-# only the first time to generate me.addr + me.sk
-# then request test ADA from faucet page -> me.addr
-mkdir keys
-python generate-credentials.py
-mv me.* keys/
+# only the first time
+$ mkdir keys
+$ python generate-credentials.py
+$ tree keys
+keys
+├── me.addr
+└── me.sk
+
+1 directory, 2 files
+
+$ # request test ADA from faucet page -> me.addr
+
+$ ./hello-world-lock.py 
+2 tADA locked into the contract
+        Tx ID: 3a92c59992213eec9d6e976d4539e122b4eac144a7622efbcbfcc34064250049
+        Datum: d8799f581ca4c617ec067044027bbb364474efa617bf13eddcde1909eef3d2d50bff
+
+$ # wait say 30sec for tx to go through
+
+$ ./hello-world-unlock.py 3a92c59992213eec9d6e976d4539e122b4eac144a7622efbcbfcc34064250049
+2 tADA unlocked from the contract
+        Tx ID: 7b929674e0b6a9a848fb7949e6c878c8178fe2b5bfd90d04c60f1f75f4a5da8a
+        Redeemer: 840000d8799f4d48656c6c6f2c20576f726c6421ff821978fd1a009aedf9
 ```
--- milestone2/investigate/aiken-pycardano/flake.nix
+++ milestone2/investigate/aiken-pycardano/flake.nix
@@ -1,60 +1,43 @@
 {
-  description = "Aiken + PyCardano env";
-
-  # based on https://www.zknotes.com/page/python%20development%20flake
-
-  # to activate, type `nix develop` while in the repo dir or a subdir.
-  # or use direnv to automatically do so (see .envrc)
-  # for best results, don't have a global python installed.  mixing python
-  # versions can make for venv problems.
+  description = "hello world with aiken + pycardano";
 
   inputs = {
-    nixpkgs.url     = "github:NixOS/nixpkgs/nixos-24.11";
-    aiken.url       = "github:aiken-lang/aiken/v1.1.10";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    aiken.url   = "github:aiken-lang/aiken/v1.1.19";
   };
 
-  outputs = { self, nixpkgs, aiken, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pname = "aiken + python dev environment";
-        pkgs = nixpkgs.legacyPackages."${system}";
-        venvDir = ".venv";
-      in
-        rec {
-          inherit pname;
+  outputs = { self, nixpkgs, aiken }@inputs:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux.extend py312Overlay;
 
-          # `nix develop`
-          devShell = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              aiken.packages.x86_64-linux.aiken
-              python3Packages.python
-              python3Packages.python-lsp-server
-              python3Packages.autopep8
-            ];
-
-            shellHook = ''
-                # create a virtualenv if there isn't one.
-
-                # DOESN'T install deps for the python app.  Do that once `nix develop` runs with
-                # $ cd src
-                # $ pip install -r ./requirements.txt
-
-                if [ -d "${venvDir}" ]; then
-                  echo "Skipping venv creation, '${venvDir}' already exists"
-                else
-                  echo "Creating new venv environment in path: '${venvDir}'"
-                  # Note that the module venv was only introduced in python 3, so for 2.7
-                  # this needs to be replaced with a call to virtualenv
-                  python -m venv "${venvDir}"
-                  # unescape to attempt use
-                  # \$\{pythonPackages.python.interpreter\} -m venv "${venvDir}"
-                fi
-
-                # activate our virtual env.
-                source "${venvDir}/bin/activate"
-              '';
+      py312Overlay = self: super: {
+        python312 = super.python312.override {
+          packageOverrides = pyself: pysuper: {
+            cbor2     = pyself.callPackage ./nix-packages/cbor2.nix     {};
+            pycardano = pyself.callPackage ./nix-packages/pycardano.nix {};
           };
-        }
-    );
+        };
+      };
+
+      devPkgList = ps: with ps; [
+        file
+        jq
+        time
+        tree
+      ];
+
+      helloPyPkgList = ps: with ps; [
+        pycardano
+      ];
+
+      in
+        {
+          # `nix develop`
+          devShells.x86_64-linux.default = pkgs.mkShell {
+            nativeBuildInputs = devPkgList pkgs ++ (with pkgs; [
+              aiken.packages.x86_64-linux.aiken
+              (pkgs.python312.withPackages helloPyPkgList)
+            ]);
+          };
+        };
 }
