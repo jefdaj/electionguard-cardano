26921f08fe46b57e1f345805caf0676e950cd8d5
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-07-31 11:19:08 -0700

    wip custom OutputReferenceHack cbor encoding

--- milestone2/pubsub2-aiken/onchain/test.py
+++ milestone2/pubsub2-aiken/onchain/test.py
@@ -17,66 +17,22 @@ import tempfile
 from pprint import pprint
 import subprocess
 
-def pick_oneshot_ref(context, addr):
+# This is a temporary hack for use with `aiken blueprint apply`
+# See https://github.com/Python-Cardano/pycardano/issues/439
+# TODO revisit once native apply_params support is released
+@dataclass
+class OutputReferenceHack(PlutusData):
+    CONSTR_ID = 0
+    transaction_id: bytes
+    index: int
+
+def pick_oneshot_utxo(context, addr):
+    # No particular logic to max here; any UTXO should work for the initial tests
     utxos = context.utxos(addr)
     if not utxos:
         raise Exception(f'addr {addr} has no UTXOs')
-    largest_utxo = max(utxos, key=lambda utxo: utxo.output.amount.coin)
-    # print(largest_utxo)
-    # print(largest_utxo.to_cbor().hex())
-    oref = f'{largest_utxo.input.transaction_id}#{largest_utxo.input.index}'
-    return oref
-
-# def parameterize_aiken_script(
-#     project_path: str,
-#     validator_name: str,
-#     params: list[tuple[str, str]]  # [(param_name, param_value), ...]
-# ):
-#     with TemporaryDirectory() as tmpdir:
-#         copytree(project_path, tmpdir, dirs_exist_ok=True)
-# 
-#         apply_params = [f"-p {k}={v}" for k, v in params]
-#         cmd = [
-#             "aiken", "blueprint", "apply",
-#             *apply_params
-#         ]
-#         print(cmd)
-# 
-#         apply_result = subprocess.run(
-#             cmd,
-#             cwd=tmpdir,
-#             capture_output=True,
-#             text=True
-#         )
-# 
-#         if apply_result.returncode != 0:
-#             raise RuntimeError(f"Aiken apply failed: {apply_result.stderr}")
-# 
-#         build_result = subprocess.run(
-#             ["aiken", "build"],
-#             cwd=tmpdir,
-#             capture_output=True,
-#             text=True
-#         )
-# 
-#         if build_result.returncode != 0:
-#             raise RuntimeError(f"Aiken build failed: {build_result.stderr}")
-# 
-#         # Load the Plutus JSON
-#         # plutus_json_path = os.path.join(tmpdir, 'build', 'plutus.json')
-#         # with open(plutus_json_path, 'r') as f:
-#         #     plutus_json = json.load(f)
-#         # Find the specific validator
-#         # validator = next(
-#         #     (v for v in plutus_json['validators']
-#         #      if v['title'] == validator_name),
-#         #     None
-#         # )
-#         # if not validator:
-#         #     raise ValueError(f"Validator {validator_name} not found")
-#         # return validator['compiledCode']
-
-# def parameterize_aiken_script(plutus_json_path: str, params: list[str]):
+    utxo = max(utxos, key=lambda utxo: utxo.output.amount.coin)
+    return utxo
 
 def parameterize_blueprint(plutus_json_path, parameters):
     """
@@ -150,12 +106,19 @@ def main():
     signing_key = PaymentSigningKey.load("keys/me.sk")
     owner = PaymentVerificationKey.from_signing_key(signing_key).hash()
     addr = read_addr('keys/me.addr')
-    print(addr)
-    # oneshot_ref = pick_oneshot_ref(context, addr)
-    channel_name = cbor2.dumps(b'test1_channel').hex() # note the b!
-    print('channel_name:', channel_name)
-    oneshot_ref = pick_oneshot_ref(context, addr)
-    print('oneshot_ref:', oneshot_ref)
+    print('addr:', addr)
+
+    channel_hex = cbor2.dumps(b'test1_channel').hex() # note the b!
+    print('channel_hex:', channel_hex)
+
+    oneshot_utxo = pick_oneshot_utxo(context, addr)
+    oneshot_ref = OutputReferenceHack(
+        oneshot_utxo.input.transaction_id.to_cbor(),
+        oneshot_utxo.input.index
+    )
+    oneshot_hex = oneshot_ref.to_cbor().hex()
+    print('oneshot_hex:', oneshot_hex)
+
     # validator = read_validator()
     # print(
     #     f"2 tADA locked into the contract\n\tTx ID: {tx_hash}\n\tDatum: {datum.to_cbor_hex()}"
@@ -164,11 +127,9 @@ def main():
     # TODO try parameterizing with one of the js libraries and see what cbor it generates!
     #      then mimic the same thing in python
 
-    # fails because i haven't figured out how to encode the oneshot_ref properly:
-    # script_cbor = parameterize_blueprint('plutus.json', [channel_name, oneshot_ref])
-
-    script_cbor = parameterize_blueprint('./plutus.json', [channel_name])
-    # pprint(script_cbor)
+    script_cbor = parameterize_blueprint('./plutus.json', [channel_hex, oneshot_hex])
+    print('script_cbor:')
+    pprint(script_cbor)
 
 if __name__ == '__main__':
     main()
