14a97777c8ee6d7258765e38f41f83814dcb5398
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-07-31 17:47:36 -0700

    wip pubsub2 test close partly working

--- milestone2/pubsub2-aiken/onchain/test.py
+++ milestone2/pubsub2-aiken/onchain/test.py
@@ -183,7 +183,7 @@ def open_channel(
     # print('assets:', assets)
 
     mint_tx = (
-        TransactionBuilder(ctx, mint=assets) # TODO required_signers=[sk] too?
+        TransactionBuilder(ctx, mint=assets)
         .add_minting_script(script=script, redeemer=psopen)
         # .add_input_address(addr)
         .add_input(oneshot_utxo)
@@ -196,6 +196,68 @@ def open_channel(
     print('seems successful?')
     # TODO put back submit
     # ctx.submit_tx(mint_tx_signed)
+
+def close_channel(
+    ctx: OgmiosV6ChainContext,
+    sk: PaymentSigningKey,
+    addr: Address,
+    channel_bytes: bytes
+):
+    channel_hex = cbor2.dumps(channel_bytes).hex()
+    # print('channel_hex:', channel_hex)
+
+    # oneshot_utxo = pick_oneshot_utxo(ctx, addr)
+    # TODO discover this from on-chain context? or just save when opening channel
+    oneshot_ref = OutputReferenceHack(
+        bytes.fromhex("7b929674e0b6a9a848fb7949e6c878c8178fe2b5bfd90d04c60f1f75f4a5da8a"),
+        1
+    )
+    oneshot_hex = oneshot_ref.to_cbor().hex()
+    print('oneshot_hex:', oneshot_hex)
+
+    script_json = aiken_blueprint_apply_hex_params(
+        './plutus.json',
+        [channel_hex, oneshot_hex]
+    )
+    # print('script_json:', script_json)
+
+    # TODO is this intermediate dict format helpful?
+    script_compiled = validator_bytes_and_hash(script_json)
+    script = PlutusV3Script(script_compiled['script_bytes'])
+
+    psclose = Redeemer(data=PsClose())
+
+    # the quicker from_primitive way has some normalize error here
+    channel_nft = AssetName(channel_bytes)
+    asset = Asset()
+    asset[channel_nft] = -1
+    assets = MultiAsset()
+    policy_id = script_hash(script)
+    assets[policy_id] = asset
+    # print('assets:', assets)
+
+    # TODO discover this from on-chain context
+    channel_nft_input = TransactionInput(
+        TransactionId(bytes.fromhex('eac80f1752ce0aa5286fb1441efd6c6bd3eee3aaaa81761ecd73349613cd304e')),
+        0
+    )
+
+    # TODO OK this is almost working! I think lol.
+    # It seems to be having trouble with coin selection, but otherwise passes.
+
+    mint_tx = (
+        TransactionBuilder(ctx, mint=assets)
+        .add_minting_script(script=script, redeemer=psclose)
+        .add_input_address(addr)
+        # .add_input(channel_nft_input)
+    )
+    # print(mint_tx)
+
+    mint_tx_signed = mint_tx.build_and_sign([sk], change_address=addr)
+    # print('mint_tx_signed:', mint_tx_signed)
+
+    # print('seems successful?')
+    ctx.submit_tx(mint_tx_signed)
 
 
 def main():
@@ -211,9 +273,11 @@ def main():
 
     # this is used to parameterize the validator,
     # and also to name the channel nft
+    # TODO is it not needed as a parameter? maybe only oneshot_ref is ok
     channel_bytes = b'test channel 001'
 
-    open_channel(ctx, sk, addr, channel_bytes)
+    # open_channel(ctx, sk, addr, channel_bytes)
+    close_channel(ctx, sk, addr, channel_bytes)
 
 if __name__ == '__main__':
     main()
