4de4cd1a7c26cbd1a2bfa95695dd0d3d04986455
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-08-18 10:04:22 -0700

    pubsub3 nix flake update

--- milestone2/pubsub3-aiken-meshjs/flake.nix
+++ milestone2/pubsub3-aiken-meshjs/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "pubsub dApp test #2: ipfs, aiken, pycardano";
+  description = "pubsub dApp test #3: ipfs, aiken, pycardano";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
