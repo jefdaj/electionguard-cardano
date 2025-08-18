c1de32793a4bce83239bb34323d2847d3b9410e8
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-08-18 11:04:47 -0700

    wip aiken-meshjs hello world

--- milestone2/investigate/aiken-meshjs/src/common.ts
+++ milestone2/investigate/aiken-meshjs/src/common.ts
@@ -9,22 +9,19 @@ import { applyParamsToScript } from "@meshsdk/core-cst";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const blockfrost_api_key = process.env.BLOCKFROST_API_KEY || "";
-export const wallet_mnemonic = process.env.MNEMONIC
-  ? process.env.MNEMONIC.split(",")
-  : "solution,".repeat(24).split(",").slice(0, 24);
-
-const blockchainProvider = new BlockfrostProvider(blockfrost_api_key);
-
 export const wallet = new MeshWallet({
   networkId: 0,
   fetcher: blockchainProvider,
   submitter: blockchainProvider,
   key: {
-    type: "mnemonic",
-    words: wallet_mnemonic,
+    type: "root",
+    bech32: fs.readFileSync("keys/me.sk").toString(),
   },
 });
+
+// TODO rewrite this to use my ogmios
+export const blockfrost_api_key = process.env.BLOCKFROST_API_KEY || "";
+const blockchainProvider = new BlockfrostProvider(blockfrost_api_key);
 
 export async function getWalletInfoForTx() {
   const utxos = await wallet.getUtxos();
