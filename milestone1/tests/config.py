9151df55195e22b13ca4c2ccc9e33dfea2ecd041
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-12-20 13:31:27 -0800

    device_mutate_spoiled_ballot

--- milestone1/tests/config.py
+++ milestone1/tests/config.py
@@ -22,9 +22,7 @@ ATTACKS = {
     'device_withhold_spoiled_ballot'   : {'who': 'device', 'when': ['vote_reveal_all']},
     'guardian_withhold_tally_share'    : {'who': 'guardian', 'when': ['decrypt_shares']},
     'guardian_withhold_spoiled_share'  : {'who': 'guardian', 'when': ['decrypt_shares']},
-
-    # Things to finish:
-    'device_mutate_submitted_ballot'     : {'who': 'device', 'when': ['vote_reveal_all']},
+    'device_mutate_spoiled_ballot'     : {'who': 'device', 'when': ['vote_reveal_all']},
 
     # Things not verified in the current implementation:
     # 'admin_mutate_constants'            : {'who': 'admin' , 'when': ['build_election']},
