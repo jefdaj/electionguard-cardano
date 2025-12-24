c6e62b25df2d774e7d932755679a23fc55918c01
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-12-24 12:15:00 -0800

    pubsub1-ipfs publish.py debounce uploads

--- milestone2/pubsub1-ipfs/publisher/publish.py
+++ milestone2/pubsub1-ipfs/publisher/publish.py
@@ -49,6 +49,7 @@ def wrapped_json_with_path(actual_path: str, virtual_path: str) -> dict:
 
 
 async def upload_and_announce_json(actual_path: str, virtual_path: str, new_cids_path: str):
+    print(f'upload_and_announce_json {actual_path}')
     try:
         client = aioipfs.AsyncIPFS(maddr=IPFS_API_ADDR)
         js = wrapped_json_with_path(actual_path, virtual_path)
@@ -66,56 +67,85 @@ async def upload_and_announce_json(actual_path: str, virtual_path: str, new_cids
     except Exception as e:
         print(e)
 
-
 class UploadNewFiles(FileSystemEventHandler):
-    def __init__(self, loop, upload_dir, new_cids_path, *args, **kwargs):
+    def __init__(self, loop, upload_dir, new_cids_path, debounce_seconds=1.0, *args, **kwargs):
         super(UploadNewFiles, self).__init__(*args, **kwargs)
         self.loop = loop
         self.upload_dir = upload_dir
         self.new_cids_path = new_cids_path
+        self.debounce_seconds = debounce_seconds
+        self.pending_uploads = {}  # path -> asyncio.Handle
 
-    def upload(self, path: str):
+    async def upload(self, path: str):
         virtual_path = os.path.splitext(
             os.path.relpath(path, self.upload_dir)
         )[0]
-        # print('upload:', locals())
         try:
-            self.loop.run_until_complete(
-                upload_and_announce_json(
-                    path, virtual_path, self.new_cids_path
-                )
+            await upload_and_announce_json(
+                path, virtual_path, self.new_cids_path
             )
         except Exception as e:
             print(e)
             raise
+        finally:
+            self.pending_uploads.pop(path, None)
+
+    def schedule_upload(self, path: str):
+        print(f'schedule_upload {path}')
+
+        # Cancel existing pending upload if any
+        if path in self.pending_uploads:
+            self.pending_uploads[path].cancel()
+
+        async def delayed_upload():
+            await asyncio.sleep(self.debounce_seconds)
+            await self.upload(path)
+            self.pending_uploads.pop(path, None)
+
+        # Use call_soon_threadsafe to schedule from another thread
+        future = asyncio.run_coroutine_threadsafe(
+            delayed_upload(),
+            self.loop
+        )
+        self.pending_uploads[path] = future
 
     def on_created(self, event):
         path = event.src_path
+        print(f'on_created {path}')
         if not os.path.isfile(path):
             return
-        print(f'File {path} has been created')
-        self.upload(path)
+        self.schedule_upload(path)
 
     def on_modified(self, event):
         path = event.src_path
+        print(f'on_modified {path}')
         if not os.path.isfile(path):
             return
-        print(f'File {path} has been modified')
-        self.upload(path)
+        self.schedule_upload(path)
 
 
 if __name__ == '__main__':
+
     upload_dir    = sys.argv[1]
-    os.makedirs(upload_dir, exist_ok=True)
     new_cids_path = sys.argv[2]
+
+    os.makedirs(upload_dir, exist_ok=True)
+
     loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
     observer = Observer()
     uploader = UploadNewFiles(loop, upload_dir, new_cids_path)
     observer.schedule(uploader, path=upload_dir, recursive=True)
     observer.start()
+
     try:
+        loop.run_forever()
         while True:
             time.sleep(1)
-    except: # TODO sigint?
+    except: # TODO sigint? keyboardinterrupt?
+        pass
+    finally:
         observer.stop()
-    observer.join()
+        observer.join()
+        loop.close()
