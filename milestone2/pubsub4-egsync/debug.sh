804180c4bffc8dc93f8e748cb20717a41d9119e0
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-12-30 20:15:56 -0800

    debug.sh working thru add_devices

--- milestone2/pubsub4-egsync/debug.sh
+++ milestone2/pubsub4-egsync/debug.sh
@@ -23,4 +23,6 @@ sudo ./election.py --project-config election.json --logfile election.log --rando
 sleep 10
 sudo ./election.py --project-config election.json --logfile election.log --random-seed 1 --single-step mint_device_channels
 sleep 10
-sudo ./election.py --project-config election.json --logfile election.log --random-seed 1 --single-step teardown
+sudo ./election.py --project-config election.json --logfile election.log --random-seed 1 --single-step add_devices
+# sleep 10
+# sudo ./election.py --project-config election.json --logfile election.log --random-seed 1 --single-step teardown
