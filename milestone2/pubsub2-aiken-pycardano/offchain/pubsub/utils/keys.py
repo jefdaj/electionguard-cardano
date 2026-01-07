e6dd39e5bc410449421d4edec80f3639f51ef46f
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-01-06 22:27:13 -0800

    wip pubsub2 keys

--- milestone2/pubsub2-aiken-pycardano/offchain/pubsub/utils/keys.py
+++ milestone2/pubsub2-aiken-pycardano/offchain/pubsub/utils/keys.py
@@ -1,6 +1,45 @@
+from os import makedirs
+from os.path import basename, exists, join, realpath
 from pathlib import Path
+from pycardano import Address, Network, SigningKey, PaymentSigningKey, PaymentVerificationKey
 
-def load_signing_key():
-    sk_path = Path("./keys/old-me.sk")
-    with open(sk_path, 'r') as f:
-        return f.read()
+KEYS_DIR = Path(__file__).parent / '../../keys'
+SIGNING_KEY = join(KEYS_DIR, 'pubsub2.sk')
+PUBLIC_ADDR = join(KEYS_DIR, 'pubsub2.addr')
+
+def generate_keys():
+    if exists(SIGNING_KEY):
+        assert exists(PUBLIC_ADDR)
+        return
+    makedirs(KEYS_DIR, exist_ok=True)
+    signing_key = PaymentSigningKey.generate()
+    signing_key.save(SIGNING_KEY)
+    verification_key = PaymentVerificationKey.from_signing_key(signing_key)
+    address = Address(payment_part=verification_key.hash(), network=Network.TESTNET)
+    with open(PUBLIC_ADDR, "w") as f:
+        f.write(str(address))
+    msg = f'''
+    Your new Preview testnet keys are here:
+
+    {realpath(SIGNING_KEY)}
+    {realpath(PUBLIC_ADDR)}
+
+    Your public address (2nd file) is: {address}
+
+    Before continuing, fund that address with tADA from the faucet:
+    https://docs.cardano.org/cardano-testnets/tools/faucet
+
+    If you don't, local tests will still work but testnet tests will fail.
+    '''
+    input(msg)
+ 
+def load_test_wallet_addr() -> Address:
+    generate_keys()
+    with open(PUBLIC_ADDR, 'r') as f:
+        return Address.from_primitive(f.read())
+
+def load_test_wallet_signing_key() -> SigningKey:
+    generate_keys()
+    with open(SIGNING_KEY, 'r') as f:
+        # TODO would validate_type=True here help?
+        return SigningKey.from_json(f.read())
