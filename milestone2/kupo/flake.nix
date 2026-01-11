62fd19bf791d5c63c723e9eed0f9f4eda1a69b85
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-01-11 10:39:42 -0800

    pubsub2 update kupo and nix versions

--- milestone2/kupo/flake.nix
+++ milestone2/kupo/flake.nix
@@ -1,6 +1,6 @@
 {
   description = "ipfs-cluster test env";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   outputs = { self, nixpkgs, ... }: {
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
     devShells.x86_64-linux.default = self.pkgs.mkShell {
