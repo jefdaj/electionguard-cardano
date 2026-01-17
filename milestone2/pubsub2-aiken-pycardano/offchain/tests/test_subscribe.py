6c85cacdbedb370f49adcfe91c2f78e6da488712
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-01-16 16:29:03 -0800

    wip pubsub2 kupo quirks

--- milestone2/pubsub2-aiken-pycardano/offchain/tests/test_subscribe.py
+++ milestone2/pubsub2-aiken-pycardano/offchain/tests/test_subscribe.py
@@ -19,13 +19,13 @@ def sub_closed(pub_closed: Publisher):
     cfg = SubscriberConfig(
         since['slot'],
         since['block_hash'],
-        since['slot'] + 600, # ~10min
+        since['slot'] + 30,
         pub_closed.pubsub_script.policy_id
     )
     sub = Subscriber(cfg, handle_match)
     sub.start() # TODO should this happen automatically?
     # TODO how to properly run it and await result?
-    sleep(120) # 2min
+    sleep(30) # 2min
     sub.stop()
     return sub
 
@@ -36,23 +36,23 @@ def sub_pub1(pub_pub1: Publisher):
     cfg = SubscriberConfig(
         since['slot'],
         since['block_hash'],
-        since['slot'] + 60,
+        since['slot'] + 30,
         pub_pub1.pubsub_script.policy_id
     )
     sub = Subscriber(cfg, handle_match)
     sub.start() # TODO should this happen automatically?
     # TODO how to properly run it and await result?
-    sleep(120)
+    sleep(30)
     sub.stop()
     return sub
 
 
 # TODO sub_open
-# TODO sub_pub1
 # TODO sub_pub2
 # TODO sub_all
 
-@pytest.fixture
+# TODO actually is this one per session?
+@pytest.fixture(scope="function")
 def cfg_all() -> SubscriberConfig:
     "Config for subscribing to a historical channel with 81 published CIDs"
     return SubscriberConfig(
@@ -63,26 +63,32 @@ def cfg_all() -> SubscriberConfig:
         # TODO until_slot not needed because we know this one was closed?
     )
 
-### tests ###
-
-# TODO separate mark for subscribe vs publish?
-@pytest.mark.testnet
-@pytest.mark.slow
-def test_subscribe_all_hardcoded(
-    cfg_all: SubscriberConfig,
-    election_records: list[tuple[Path, bytes]]
-):
-    "Subscribe to a hardcoded channel with all 81 election record CIDs"
+@pytest.fixture(scope="function")
+def sub_all(cfg_all: SubscriberConfig) -> Subscriber:
     sub = Subscriber(cfg_all, handle_match)
     sub.start()
     # TODO come up with a better wait mechanism
     # while not sub._watcher_stop.is_set():
     sleep(30)
     sub.stop()
+    return sub
+
+
+### tests ###
+
+# TODO separate mark for subscribe vs publish?
+@pytest.mark.testnet
+@pytest.mark.slow
+def test_subscribe_all_hardcoded(
+    sub_all: Subscriber,
+    election_records: list[tuple[Path, bytes]]
+):
+    "Subscribe to a hardcoded channel with all 81 election record CIDs"
     cids = list(v for (k, v) in election_records)
-    assert sub.subscribed_cids() == cids, "Subscriber CIDs should match election_records"
+    assert sub_all.subscribed_cids() == cids, "Subscriber CIDs should match election_records"
 
 # TODO double check pytest isn't creating two different pub_closed instances here
+# TODO actually, remove pub_closed entirely?
 @pytest.mark.slow
 @pytest.mark.testnet
 def test_subscribe_closed(
