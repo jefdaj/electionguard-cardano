6f7413d5bbbeee16834db4ea76607234f1b0712b
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-02-11 20:33:16 -0800

    add check to build.sh

--- milestone2/smart-contract/onchain/build.sh
+++ milestone2/smart-contract/onchain/build.sh
@@ -2,5 +2,6 @@
 
 set -x
 cd $(dirname "$0")
+aiken check # don't want to generate the JSON if it's failing a test
 aiken build --out election-plutus.json
 aiken build --out election-plutus-traced.json --trace-level verbose
