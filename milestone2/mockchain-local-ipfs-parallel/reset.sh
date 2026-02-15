6f735518aeee5002c162777a47b3a9e60206e659
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-02-15 09:26:20 -0800

    add reset.sh to reset after failed tests

---
+++ milestone2/mockchain-local-ipfs-parallel/reset.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -x
+[[ $(docker ps | wc -l) -gt 1 ]] && (docker ps | grep 'test' | awk '{print $1}' | xargs docker stop)
+[[ $(docker network ls | grep test | wc -l) -gt 0 ]] && (docker network ls| grep test | awk '{print $1}' | xargs docker network rm)
+[[ -d data || -d tests ]] && sudo rm -rf data tests
