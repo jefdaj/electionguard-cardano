1677bdd8d789ee7b13996cfecfed63ec1cb71975
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-02-18 09:13:12 -0800

    wip init pubsub3

--- milestone2/pubsub3/onchain/build.sh
+++ milestone2/pubsub3/onchain/build.sh
@@ -2,5 +2,6 @@
 
 set -x
 cd $(dirname "$0")
+aiken check # don't want to generate the JSON if it's failing a test
 aiken build --out pubsub3-plutus.json
 aiken build --out pubsub3-plutus-traced.json --trace-level verbose
