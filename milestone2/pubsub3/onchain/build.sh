439db96c193e025d60fbbb90b9268d8b73de5282
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-03-04 10:17:40 -0800

    wip adding subchannels

--- milestone2/pubsub3/onchain/build.sh
+++ milestone2/pubsub3/onchain/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 set -x
-cd $(dirname "$0")
 set -e
+cd $(dirname "$0")
 aiken check # don't want to generate the JSON if it's failing a test
 aiken build --out election-plutus.json
 aiken build --out election-plutus-traced.json --trace-level verbose
