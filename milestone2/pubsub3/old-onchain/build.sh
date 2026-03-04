10450050f1488a8360f6d3a4b6d2d78d8a6d9ccd
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-03-04 08:34:57 -0800

    wip merge smart-contract-parallel into pubsub3

--- milestone2/pubsub3/old-onchain/build.sh
+++ milestone2/pubsub3/old-onchain/build.sh
@@ -3,5 +3,5 @@
 set -x
 cd $(dirname "$0")
 aiken check # don't want to generate the JSON if it's failing a test
-aiken build --out election-plutus.json
-aiken build --out election-plutus-traced.json --trace-level verbose
+aiken build --out pubsub3-plutus.json
+aiken build --out pubsub3-plutus-traced.json --trace-level verbose
