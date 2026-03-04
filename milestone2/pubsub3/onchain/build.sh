d8144b2f49dfdf8bcca0ae33236cd6ace7f5b998
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-03-04 09:21:54 -0800

    build.sh set -e

--- milestone2/pubsub3/onchain/build.sh
+++ milestone2/pubsub3/onchain/build.sh
@@ -2,6 +2,7 @@
 
 set -x
 cd $(dirname "$0")
+set -e
 aiken check # don't want to generate the JSON if it's failing a test
 aiken build --out election-plutus.json
 aiken build --out election-plutus-traced.json --trace-level verbose
