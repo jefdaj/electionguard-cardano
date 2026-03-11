2a2ee39db6d94833a48e46614c5cb137f40d4e17
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-03-11 11:28:20 -0700

    wip aiken_static_data.py

--- milestone2/mockchain-local-ipfs-parallel/aiken_static_data.py
+++ milestone2/mockchain-local-ipfs-parallel/aiken_static_data.py
@@ -10,15 +10,22 @@
 # exit
 #
 # nix develop .#egsync
-# ./aiken_static_data.py
+# ./aiken_static_data.py > staticdata.ak
 
 from multiformats_cid import cid, make_cid
 from pathlib import Path
 import json
+import re
 
 IN_DIR = 'aiken_static_data'
 IN_LOG = Path(IN_DIR) / 'egsync.log'
 OUT_AK = 'aiken_static_data.ak'
+
+print('''
+// Generated with aiken_static_data.py
+// Consider editing and re-running that to make changes''')
+
+VARS = {}
 
 with open(IN_LOG, 'r') as f:
     lines = f.readlines()
@@ -44,33 +51,34 @@ const {n} = er.PublicRecord {{
   cid: {s},
   metadata: er.{m}
 }}""")
+    return n
 
 def manifest(n, j, c, s):
     m = 'Manifest'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 def ceremony_details(n, j, c, s):
     m = 'CeremonyDetails'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 def guardian_pubkey(n, j, c, s):
     i = int(j["guardian_id"].split('_')[-1])
     m = f'GuardianPubkey {{ guardian_number: {i} }}'
     n = f'guardian{i}_pubkey'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 def summary(n, j, c, s):
     i = j["verifier_id"].replace('_', '') # TODO leave underscore?
     n = f'{i}_summary'
     i = f'string.to_bytearray(@"{i}")' # TODO remove?
     m = f'Summary {{ verifier_id: {i} }}'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 def tally_share(n, j, c, s):
     i = int(j["guardian_id"].split('_')[-1])
     m = f'TallyShare {{ guardian_number: {i} }}'
     n = f'guardian{i}_tally_share'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 def spoiled_share(n, j, c, s):
     n = ballot_name('spoiled_share', j, 'spoiled_id')
@@ -82,42 +90,42 @@ def spoiled_share(n, j, c, s):
     guardian_number: {i},
     spoiled_id: {i2},
   }}'''
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 def guardian_backup(n, j, c, s):
     i = int(j["guardian_id"].split('_')[-1])
     b = int(j["backup_order"])
     m = f'GuardianBackup {{ guardian_number: {i}, backup_order: {b} }}'
     n = f'guardian{i}_backup{b}'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 def guardian_verification(n, j, c, s):
     i = int(j["guardian_id"].split('_')[-1])
     b = int(j["backup_order"])
     m = f'GuardianVerification {{ guardian_number: {i}, backup_order: {b} }}'
     n = f'guardian{i}_verification{b}'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 def joint_key(n, j, c, s):
     m = 'JointKey'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 def constants(n, j, c, s):
     m = 'Constants'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 def ciphertext_tally(n, j, c, s):
     m = 'CiphertextTally'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 def plaintext_tally(n, j, c, s):
     m = 'PlaintextTally'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 def device(n, j, c, s):
     i = int(j["device_number"])
     m = f'Device {{ device_number: {i} }}'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 def ballot_name(prefix, j, key='ballot_id'):
     i = j[key]
@@ -129,14 +137,14 @@ def ballot_submitted(n, j, c, s):
     n = ballot_name('ballot_submitted', j)
     i = f'string.to_bytearray(@"{i}")'
     m = f'BallotSubmitted {{ ballot_id: {i} }}'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 def spoiled_result(n, j, c, s):
     i = j["ballot_id"]
     n = ballot_name('spoiled_result', j)
     i = f'string.to_bytearray(@"{i}")'
     m = f'SpoiledResult {{ ballot_id: {i} }}'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 
 def ballot_spoiled(n, j, c, s):
@@ -144,14 +152,14 @@ def ballot_spoiled(n, j, c, s):
     n = ballot_name('ballot_spoiled', j)
     i = f'string.to_bytearray(@"{i}")'
     m = f'BallotSpoiled {{ ballot_id: {i} }}'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 def cast_notice(n, j, c, s):
     i = j["ballot_id"]
     n = ballot_name('cast_notice', j)
     i = f'string.to_bytearray(@"{i}")'
     m = f'CastNotice {{ ballot_id: {i} }}'
-    record(n, c, s, m)
+    return record(n, c, s, m)
 
 render_fns = [
     manifest,
@@ -174,6 +182,8 @@ render_fns = [
     summary,
 ]
 
+VARNAMES = set()
+
 # generate individual records
 for j in JSONS:
     for fn in render_fns:
@@ -181,8 +191,32 @@ for j in JSONS:
         cid_str = convert_cid_for_aiken(j["cid"])
         fn_name = fn.__name__
         if j["record_type"] == fn.__name__:
-            fn(fn_name, j, comment, cid_str)
+            varname = fn(fn_name, j, comment, cid_str)
+            VARNAMES.add(varname)
         # print(json.dumps(j, indent=2))
         # print(j["record_type"])
 
-# TODO generate lists?
+VARNAMES = sorted(VARNAMES)
+
+# generate lists
+lists = {
+    'admin' : '(manifest|joint_key|ceremont_details|ciphertext_tally|plaintext_tally)',
+    'guardian1' : 'guardian1',
+    'guardian2' : 'guardian2',
+    'guardian3' : 'guardian3',
+    'ballot_submitted' : 'ballot_submitted',
+    'ballot_spoiled' : 'ballot_spoiled',
+    'spoiled_share' : 'spoiled_share',
+    'spoiled_result' : 'spoiled_result',
+    'device1' : '(device|ballot_submitted|ballot_spoiled|cast_notice)',
+    'verifier1' : 'verifier1',
+    'summary' : '.*_summary',
+}
+
+for list_name, list_regex in lists.items():
+    print()
+    print(f'const {list_name}_records = [')
+    for v in VARNAMES:
+        if re.match(list_regex, v):
+            print(f'  {v},')
+    print(']')
