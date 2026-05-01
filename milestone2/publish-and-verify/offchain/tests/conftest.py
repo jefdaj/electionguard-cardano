4a8bf7dfd3b163228c881469a2b04f477f7c9b1e
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-05-01 13:51:41 -0700

    add test_script

--- milestone2/publish-and-verify/offchain/tests/conftest.py
+++ milestone2/publish-and-verify/offchain/tests/conftest.py
@@ -3,12 +3,13 @@ import pytest
 
 from os.path import realpath, join, exists
 from pathlib import Path
-from pycardano import OgmiosV6ChainContext, SigningKey
+from pycardano import OgmiosV6ChainContext, Address, SigningKey, VerificationKeyHash, UTxO
 from typing import Dict, List
 
 from election import ogmios as eo
 from election import wallet as ew
 from election import plutus as ep
+from election.plutus import script as es
 
 
 ### misc fixtures ###
@@ -20,16 +21,22 @@ def ogmios() -> OgmiosV6ChainContext:
     return eo.OGMIOS_CTX
 
 @pytest.fixture(scope='session')
-def addr():
+def addr() -> Address:
     '''Load test publisher address.'''
     return ew.load_election_wallet_addr()
 
 @pytest.fixture(scope='session')
-def sk():
+def sk() -> SigningKey:
     '''Load test publisher signing key.'''
     return ew.load_election_wallet_signing_key()
 
 @pytest.fixture(scope='session')
-def vkh(sk: SigningKey) -> ew.VerificationKeyHash:
+def vkh(sk: SigningKey) -> VerificationKeyHash:
     '''Load test publisher verification key hash.'''
     return ew.vkh_for_signing_key(sk)
+
+# TODO explicit scope?
+@pytest.fixture
+def oneshot_utxo(ogmios: OgmiosV6ChainContext, addr: Address) -> UTxO:
+    '''Pick a oneshot UTxO from the election wallet.'''
+    return es.pick_oneshot_utxo(ogmios, addr)
---
+++ milestone2/publish-and-verify/offchain/tests/test_script.py
@@ -0,0 +1,16 @@
+import pytest
+from .conftest import *
+from election.plutus.script import *
+
+def test_pick_oneshot_utxo(oneshot_utxo: UTxO):
+    assert isinstance(oneshot_utxo, UTxO)
+
+def test_init_script(oneshot_utxo: UTxO):
+    script = ElectionScript(oneshot_utxo)
+    assert isinstance(script, ElectionScript)
+    assert isinstance(script.mint_script, bytes)
+    assert isinstance(script.spend_script, bytes)
+    assert isinstance(script.policy_id, ScriptHash)
+    assert isinstance(script.address, Address)
+
+# TODO test save/load json
