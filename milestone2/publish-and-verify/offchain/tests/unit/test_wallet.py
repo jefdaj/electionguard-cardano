c11c76bf08a29ab0166bca217ed9efcc890c0674
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-06-02 11:44:27 -0700

    rename tests

--- milestone2/publish-and-verify/offchain/tests/unit/test_wallet.py
+++ milestone2/publish-and-verify/offchain/tests/unit/test_wallet.py
@@ -1,13 +1,13 @@
 import pytest
 from egc import *
 
-def test_funder_keys(funder_keys: KeyPair):
+def test_load_funder_keys(funder_keys: KeyPair):
     assert isinstance(funder_keys, KeyPair)
     assert isinstance(funder_keys.sk, SigningKey)
     assert isinstance(funder_keys.addr, Address)
     assert isinstance(funder_keys.vkh, VerificationKeyHash)
 
-def test_admin_keys(admin_keys: KeyPair):
+def test_create_admin_keys(admin_keys: KeyPair):
     assert isinstance(admin_keys, KeyPair)
     assert isinstance(admin_keys.sk, SigningKey)
     assert isinstance(admin_keys.addr, Address)
