67edd8db2f7c8f0acc526dcd857bae38fb7793a5
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-06-02 11:55:41 -0700

    test_admin_assets

--- milestone2/publish-and-verify/offchain/tests/unit/test_admin.py
+++ milestone2/publish-and-verify/offchain/tests/unit/test_admin.py
@@ -5,5 +5,9 @@ import logging
 LOG = logging.getLogger(__name__)
 
 def test_admin_assets(admin_assets: MultiAsset):
+    LOG.debug(f'admin_assets: {admin_assets}')
     assert isinstance(admin_assets, MultiAsset)
-    # TODO also test it has the STT
+    assert len(admin_assets) == 1
+    for script in admin_assets:
+        assert len(admin_assets[script]) == 1
+        assert AssetName(b'egc-election-admin-stt') in admin_assets[script]
