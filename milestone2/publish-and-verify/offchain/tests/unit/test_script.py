b31c2a73534172b9bb960582ccf8a026fb2d9e27
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-06-02 12:51:01 -0700

    test names

--- milestone2/publish-and-verify/offchain/tests/unit/test_script.py
+++ milestone2/publish-and-verify/offchain/tests/unit/test_script.py
@@ -1,12 +1,12 @@
 import pytest
 from egc import *
 
-def test_roundrip_utxo_to_dict(oneshot_utxo: UTxO):
+def test_roundrip_utxo(oneshot_utxo: UTxO):
     tmp = oneshot_utxo.to_cbor_hex()
     utxo2 = UTxO.from_cbor(bytes.fromhex(tmp))
     assert utxo2 == oneshot_utxo
 
-def test_roundtrip_script_to_dict(script: ElectionScript):
+def test_roundtrip_script(script: ElectionScript):
     tmp = script.to_dict()
     script2 = ElectionScript.from_dict(data=tmp)
     assert script == script2
