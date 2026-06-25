b08ed4e5c6c00eb746a77750737c3e5a32437587
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-06-25 08:20:01 -0700

    done with old-offchain now

--- milestone2/publish-and-verify/old-offchain/pubsub/plutus/__init__.py
+++
@@ -1,4 +0,0 @@
-from .builders import build_psopen_tx, build_pspublish_tx, build_psclose_tx
-from .script import PubsubScript
-from .types import CIDv1, PubsubState, PubsubAction, PsOpen, PsPublish, PsClose
-from .utils import OutputReferenceHack, pick_oneshot_utxo, utxo_to_ref_hex, aiken_blueprint_apply_hex_params
--- milestone2/publish-and-verify/old-offchain/test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-EXTRA_ARGS="$@"
-time pytest -vv $EXTRA_ARGS 2>&1 | tee test.log
