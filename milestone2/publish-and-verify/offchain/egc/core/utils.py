f0696809c94a1d06f21524fabbba463db84a297c
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-06-24 20:10:48 -0700

    safe_deepdiff needs to avoid some fancy plutus types

--- milestone2/publish-and-verify/offchain/egc/core/utils.py
+++ milestone2/publish-and-verify/offchain/egc/core/utils.py
@@ -1,18 +1,28 @@
 import dataclasses
 from deepdiff import DeepDiff
+import logging
+
+LOG = logging.getLogger(__name__)
 
 def _deepdiff_prep(obj):
     if isinstance(obj, bytes):
-        return obj.decode("utf-8", errors="replace")
+        try:
+            return obj.decode("utf-8", errors="replace")
+        except Exception as e:
+            # Will happen at least with CID fields, maybe others.
+            LOG.error(f'failed to decode bytes: {obj}')
+            return obj
     if isinstance(obj, dict):
         return {k: _deepdiff_prep(v) for k, v in obj.items()}
     if isinstance(obj, (list, tuple, set, frozenset)):
         return type(obj)(_deepdiff_prep(i) for i in obj)
     if dataclasses.is_dataclass(obj) and not isinstance(obj, type):
-        return dataclasses.replace(obj, **{
-            f.name: _deepdiff_prep(getattr(obj, f.name))
-            for f in dataclasses.fields(obj)
-        })
+        # This was problematic with the fancy PlutusData types
+        # return dataclasses.replace(obj, **{
+        #     f.name: _deepdiff_prep(getattr(obj, f.name))
+        #     for f in dataclasses.fields(obj)
+        # })
+        return obj
     # attrs support
     try:
         import attr
