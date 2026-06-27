8ee27c9843a70b32fc69d618ed298dbcaa0c7c0a
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-06-26 19:11:11 -0700

    rewrite load_static_record_pairs to fix CID order

--- milestone2/publish-and-verify/offchain/tests/data/static_records/__init__.py
+++ milestone2/publish-and-verify/offchain/tests/data/static_records/__init__.py
@@ -1,6 +1,6 @@
 from .code import STATIC_PHASES
 from .code import STATIC_TRANSACTIONS
-from egc import PublicRecord, PublicRecordMetadata, ipfs_cid_to_string
+from egc import *
 
 import json
 from pathlib import Path
@@ -11,29 +11,17 @@ import logging
 LOG = logging.getLogger(__name__)
 
 
-# TODO is absolute the best choice here?
-STATIC_FILES_DIR   = Path(__file__).absolute().parent / 'files'
+STATIC_FILES_DIR = Path(__file__).absolute().parent / 'files'
 
 
-def make_static_files_by_cid():
-    cids_path  = Path(__file__).absolute().parent / 'file_cids.txt'
-    files_path = Path(__file__).absolute().parent / 'file_paths.txt'
-    cids  = cids_path.read_text().splitlines()
-    paths = files_path.read_text().splitlines()
-    paths = [STATIC_FILES_DIR / p for p in paths]
-    fbc = {c: p for (c, p) in zip(cids, paths)}
-    for (c, p) in fbc.items():
-        LOG.debug(f'fbc pair: cid={c}, path={p}')
-    return fbc
-
-
-STATIC_FILES_BY_CID = make_static_files_by_cid()
-
-
-def load_static_record_obj(cid: str) -> dict:
-    path = STATIC_FILES_BY_CID[cid]
+def load_static_record_pair(
+        record: PublicRecord
+    ) -> tuple[dict, PublicRecordMetadata]:
+    mdata = record.metadata
+    path = record_path(mdata, STATIC_FILES_DIR)
     with open(path, 'r') as f:
-        return json.load(f)
+        obj = json.load(f)
+    return (obj, mdata)
 
 
 def load_static_record_pairs(
@@ -43,9 +31,6 @@ def load_static_record_pairs(
     pairs = []
     for rec in records:
         LOG.debug(f'rec: {rec}')
-        cid = ipfs_cid_to_string(rec.ipfs_cid)
-        LOG.debug(f'cid: {cid}')
-        obj = load_static_record_obj(cid)
-        pair = (obj, rec.metadata)
+        pair = load_static_record_pair(rec)
         pairs.append(pair)
     return pairs
