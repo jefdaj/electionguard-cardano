a547ce55369a3d61f3c0ac09035f1f4af738070c
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-07-01 19:51:25 -0700

    wip server runs, has error

--- milestone3/subscriber-ui/egc/webui/__init__.py
+++ milestone3/subscriber-ui/egc/webui/__init__.py
@@ -1,21 +1,28 @@
-from quart import Quart
+from quart import Quart, Config
+
 
 def create_app(config=None):
     app = Quart(__name__)  # static_folder/template_folder resolve to package dir
     if config:
         app.config.from_mapping(config)
-    else:
+    # else:
 
-        config = Config()
-        config.bind = ["0.0.0.0:5000"]
+    #     config = Config()
+    #     config.bind = ["0.0.0.0:5000"]
 
         # For production, increase workers based on CPU cores
         # Recommended: (2 * num_cores) + 1
-        config.workers = 1
+    #     config.workers = 1
 
-    from .blueprints.main import bp as main_bp
+    # from .blueprints.main import bp as main_bp
+    # app.register_blueprint(main_bp)
+
     from .blueprints.partials import bp as partials_bp
-    app.register_blueprint(main_bp)
     app.register_blueprint(partials_bp, url_prefix="/partials")
+
+    @app.get("/")
+    async def index():
+        return await render_template("index.html")
+
     return app
 
--- milestone3/subscriber-ui/egc/webui/blueprints/main.py
+++
@@ -1,6 +0,0 @@
-
-@app.get("/")
-async def index():
-    return await render_template("index.html")
-
-
