991fee41ed7d76df35e5b9c07619341eb710046e
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-07-03 17:47:18 -0700

    add hardcoded subscriber to app

--- milestone3/subscriber-ui/egc/webui/__init__.py
+++ milestone3/subscriber-ui/egc/webui/__init__.py
@@ -4,6 +4,9 @@ from quart import render_template, request
 # TODO remove pycardano.hash from core exports to avoid shadowing system one
 from ..core import Entry, get_log_entries, get_state_tree
 
+from ..core import ElectionSubscriber, SubscriberConfig
+from pycardano import ScriptHash
+
 def create_app(config=None):
     app = Quart(__name__)  # static_folder/template_folder resolve to package dir
     if config:
@@ -25,5 +28,19 @@ def create_app(config=None):
 
     # allow hash() to be used in templates
     app.jinja_env.globals.update(hash=hash)
+
+    # TODO get somehow rather than hardcoding
+    sub_cfg = SubscriberConfig(since_slot='116458744',
+                 since_block_hash='7c5bd72816b5e61e7814f3c385305ffc48411ab27ff2a0771d2cfae69af23943',
+                 policy_id=ScriptHash(bytes.fromhex('b8627e36b5434a5102f690785e1f22ae95093aef2d400a7cbcbb0b89')))
+
+    @app.before_serving
+    async def startup():
+        app.subscriber = ElectionSubscriber(sub_cfg) # TODO on_action
+        app.subscriber.start()
+
+    @app.after_serving
+    async def shutdown():
+        app.subscriber.stop()
 
     return app
