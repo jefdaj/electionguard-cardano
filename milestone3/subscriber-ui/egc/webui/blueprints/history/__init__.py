7250da4e5b0cdd5666957bca124d88c7839b10a0
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-07-03 19:32:43 -0700

    filter history log

--- milestone3/subscriber-ui/egc/webui/blueprints/history/__init__.py
+++ milestone3/subscriber-ui/egc/webui/blueprints/history/__init__.py
@@ -24,6 +24,10 @@ async def filter_results():
 
     q = request.args.get("history-filter", "").strip() # TODO would "query" be more standard?
     events = current_app.subscriber.all_events()
+
+    # TODO factor out into something more general
+    events = [e for e in events if q.lower() in str(e).lower()]
+
     tree = get_state_tree(query=q)
     return await render_template(
         "history/partials/filter_results.html",
