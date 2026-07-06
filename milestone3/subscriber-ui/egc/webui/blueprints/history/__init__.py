722194d67fd8973f554f1e6c01aa07dbab5ba8e4
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-07-05 17:32:57 -0700

    clean up display of event log, tree

--- milestone3/subscriber-ui/egc/webui/blueprints/history/__init__.py
+++ milestone3/subscriber-ui/egc/webui/blueprints/history/__init__.py
@@ -27,7 +27,7 @@ def build_tree(filter_str=None):
     # Note that the root node isn't currently shown. So no point having a root.html template.
     return {"id": "election", "type": "root", "label": "Election", "children": [
         {"id": "key", "type": "key_ceremony", "label": "Key ceremony", "children": [
-            {"id": "key-r1", "type": "key_round", "round": 1, "done": 2, "total": 3, "children": []},
+            {"id": "key-r1", "type": "key_round", "round": 1, "label": "Round 1", "done": 2, "total": 3, "children": []},
         ]},
         {"id": "voting", "type": "voting", "label": "Voting", "submitted": 25, "children": []},
     ]}
