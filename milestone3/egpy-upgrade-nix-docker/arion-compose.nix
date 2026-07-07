ab08c52e6775c9f9b5d45a1fac4e6e03d9c5a90a
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-07-07 13:00:19 -0700

    arion-compose permissions

--- milestone3/egpy-upgrade-nix-docker/arion-compose.nix
+++ milestone3/egpy-upgrade-nix-docker/arion-compose.nix
@@ -75,7 +75,7 @@ let
 
   egpyContainer = mode: scripts_dir: mockchain_dir: private_dir: n: {
     service.image = "ghcr.io/jefdaj/electionguard-python:1.4.0-py313.nix";
-    service.user = "1000:1000"; # TODO does this matter?
+    service.user = "1000:100"; # TODO 1000?
 
     service.volumes = [
       "${scripts_dir}:/scripts/"
