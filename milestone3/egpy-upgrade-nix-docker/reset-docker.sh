301a98ad642dee73262fc88ca1f8f7a48b6f6a5b
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-07-07 10:04:39 -0700

    reset-docker.sh

--- milestone3/egpy-upgrade-nix-docker/reset-docker.sh
+++ milestone3/egpy-upgrade-nix-docker/reset-docker.sh
@@ -3,4 +3,4 @@ set -x
 sudo id > /dev/null
 [[ $(docker ps | wc -l) -gt 1 ]] && (docker ps | grep 'test' | awk '{print $1}' | xargs docker stop)
 [[ $(docker network ls | grep test | wc -l) -gt 0 ]] && (docker network ls| grep test | awk '{print $1}' | xargs docker network rm)
-[[ -d data || -d tests ]] && sudo rm -rf data tests
+[[ -d data ]] && sudo rm -rf data
