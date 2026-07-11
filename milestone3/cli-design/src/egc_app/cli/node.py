8ea76c30592b781df13608fd03809b8bebaf46dd
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-07-11 10:06:42 -0700

    wip uvicorn run modes

--- milestone3/cli-design/src/egc_app/cli/node.py
+++ milestone3/cli-design/src/egc_app/cli/node.py
@@ -3,6 +3,7 @@ import asyncio
 import uvicorn
 from egc_app.client import Client
 from egc_app.cli.utils import RoleAwareGroup
+from egc_app.server.run import run as run_server
 
 @click.group(cls=RoleAwareGroup)
 def node() -> None:
@@ -11,19 +12,10 @@ def node() -> None:
 @node.command()
 @click.option("--host", default="0.0.0.0")
 @click.option("--port", default=8000, type=int)
-@click.option("--workers", default=1, type=int)
-@click.option("--log-level", default="info")
-def run(host, port, workers, log_level):
-    "Run the node (API server)."
-    uvicorn.run(
-        "egc_app.server.app:create_app",
-        factory=True,
-        host=host,
-        port=port,
-        reload=True,
-        workers=1, # TODO more for production?
-        log_level=log_level,
-    )
+@click.option("--dev-mode", is_flag=True, default=False)
+def run(host, port, dev_mode):
+    "Run the EGC node (API server)."
+    run_server(**locals())
 
 @node.command()
 def status():
--- milestone3/cli-design/src/egc_app/server/run.py
+++ milestone3/cli-design/src/egc_app/server/run.py
@@ -1,11 +1,23 @@
 import uvicorn
 from egc_app.server.app import create_app
 
-def main():
-    app = create_app()
-    cfg = uvicorn.Config(app, host="0.0.0.0", port=8000, reload=True)
-    server = uvicorn.Server(cfg)
-    server.run()
+def run(dev_mode: bool, **cfg_kwargs):
+    if dev_mode:
+        uvicorn.run(
+            "egc_app.server.app:create_app",
+            factory=True,
+            reload=True,
+            workers=1,
+            log_level="debug",
+            **cfg_kwargs # TODO any cfg args not accepted here?
+        )
+    else:
+        # production
+        # TODO what else should be set here?
+        app = create_app()
+        cfg = uvicorn.Config(app, **cfg_kwargs)
+        server = uvicorn.Server(cfg)
+        server.run()
 
 if __name__ == "__main__":
-    main()
+    run()
