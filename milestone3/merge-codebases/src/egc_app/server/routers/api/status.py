63f60118645209cdabfdb0bf034549627e8e90fc
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-07-13 22:18:30 -0700

    todos

--- milestone3/merge-codebases/src/egc_app/server/routers/api/status.py
+++ milestone3/merge-codebases/src/egc_app/server/routers/api/status.py
@@ -3,6 +3,8 @@ from egc_app.server.schemas.status import StatusOut
 
 router = APIRouter(tags=["status"])
 
+# TODO include ogmios status here
+# TODO include ipfs status here
 @router.get("/status", response_model=StatusOut)
 async def health():
     return StatusOut()
