0b42cfff39751407bbbe85b137c4c0d436bfb718
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-07-18 18:07:58 -0700

    start reorganizing tests

--- milestone3/merge-codebases/tests/conftest.py
+++ milestone3/merge-codebases/tests/conftest.py
@@ -20,5 +20,7 @@ def pytest_configure(config):
     # These are all probably worth looking at again if/when we have mysterious
     # API issues. But the rest of the time they're pretty noisy.
     logging.getLogger('websockets').setLevel(logging.WARNING)
-    logging.getLogger('urllib3').setLevel(logging.DEBUG)
+    logging.getLogger('urllib3').setLevel(logging.INFO)
     logging.getLogger('ogmios').setLevel(logging.WARNING)
+
+    # TODO dial down the subscriber too, either here or at the source
