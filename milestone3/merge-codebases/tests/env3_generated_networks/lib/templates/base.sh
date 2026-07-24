3da9b4e6580d9aaebaca66d376f635e94df35c28
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-07-24 10:30:26 -0700

    base.sh add cleanup trap

--- milestone3/merge-codebases/tests/env3_generated_networks/lib/templates/base.sh
+++ milestone3/merge-codebases/tests/env3_generated_networks/lib/templates/base.sh
@@ -10,10 +10,24 @@ export EGC_NODE_ROLE='{{node_role}}'
 export EGC_NODE_INDEX='{{node_index}}'
 {% endblock %}
 
+{% block cleanup %}
+cleanup() {
+  # TODO if admin, burn test tokens if any
+  # TODO return collateral
+  echo "cleaning up"
+}
+{% endblock %}
+
 {% block exittrap %}
-### exit trap ###
-# TODO if admin, burn test tokens if any
-# TODO return collateral
+### run cleanup ###
+export -f cleanup
+on_exit() {
+  local return_code=$?
+  trap - EXIT INT TERM
+  timeout -k 10s 600s bash -c cleanup || echo "cleanup failed or timed out"
+  exit $return_code
+}
+trap on_exit EXIT INT TERM
 {% endblock %}
 
 {% block body %}
