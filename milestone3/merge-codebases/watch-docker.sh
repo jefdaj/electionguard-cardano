3cd2c32689ff987a9ab41087c058dbd502bc28f1
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-07-25 09:55:08 -0700

    watch-docker.sh: 10s -> 5s

--- milestone3/merge-codebases/watch-docker.sh
+++ milestone3/merge-codebases/watch-docker.sh
@@ -4,4 +4,4 @@
 cmd="echo 'networks:'; docker network ls | grep -v NAME"
 cmd="$cmd; echo; echo 'containers:'"
 cmd="$cmd; docker stats --no-stream --format 'table {{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}' | grep -v NAME | sort -k 1 -h"
-watch -t -n10 "$cmd"
+watch -t -n5 "$cmd"
