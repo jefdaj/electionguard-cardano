af601d7a40718c81b4a898462b5b6b582da56a32
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-07-24 21:27:31 -0700

    new OLD_QR_STRS

--- milestone3/merge-codebases/tests/env3_generated_networks/test_04_subscribe.py
+++ milestone3/merge-codebases/tests/env3_generated_networks/test_04_subscribe.py
@@ -8,44 +8,16 @@ from ..lib import *
 from .lib  import *
 from ..lib.py_utils import deep_replace
 
-# weird wrapping just adds to the test!
 OLD_QR_STRS = [
 
-  # '''egc:election:3:d8799f5820dd0cae35b7b285c763543ac48f9a17334389705421
-  # 1abd51cc00591958467f2102ff:2:addr_test1vr93qqyu30r5c7snd4wp8wu243st2xz
-  # 8605yea78hgyg6uckjakk5:118279331:8bc5402b2f2db94ad2f6ae01506d141558cc4
-  # 32e60873549cb9242e1d40b7a13''',
+# init_election only
+# 'egc:election:3:d8799f5820be3f80b1c83cc2445fae4b2157a4bcf34f5a4b6d5e032f1deedb3f067dffdf6400ff:2:addr_test1vr93qqyu30r5c7snd4wp8wu243st2xz8605yea78hgyg6uckjakk5:118296189:11b99ea65fd54e194dc32fb4fbb4b76402175c24fab997db2e8861513be9c113',
 
-  # '''egc:election:3:d8799f5820fa655ccdddf7e
-  # eecc80dd21cb575d9a3e6d07a96188788cf09adfdbde6
-  # 7e565000ff:2:addr_test1vr93qqyu30r5c7snd4wp8wu243st2
-  # xz8605yea78hgyg6uckjakk5:118279418:5f2d1e4137b870aad05562
-  # cafc6ece00cd06d1dd6c968b1490bdaf3badbca430''',
+# happy_subchannels
+# 'egc:election:3:d8799f5820b2106681301173bebf1568dffde6e8bdf5b76517e51d17b527370ccb3ed14e2500ff:2:addr_test1vr93qqyu30r5c7snd4wp8wu243st2xz8605yea78hgyg6uckjakk5:118296353:5a2e81d54ad3efb5da12f46cea5fb4f22e26e09e5a5b6b18336e148236146ec0',
 
-  # '''egc:election:3:d8799f5820c1a3b60b14902823663a88602549d0
-  # 71558833fa93ad61fe2a01f424dff19f1c00ff:2
-  # :addr_test1vr93qqyu30r5c7snd4wp8wu243st2xz8605yea78hgyg6uckjakk5:
-  # 118279549:d658626596b91538146078
-  # d4fd69c332bc1b638fbfcadb47ed3237f2ee2b1d5b''',
-
-  # '''egc:
-  # election:
-  # 3:
-  # d8799f58207db31e039d39a82598e8b3d9c9b35f771a0789f974a0
-  # e7ad85b7e2cfb6dd2d8600ff:2
-  # :addr_test1vr93qqyu30r5c7snd4wp8wu243st2xz8605yea7
-  # 8hgyg6uckjakk5:118279709:69bac2384a55c72cb72123
-  # 87e77101608bb7ba2189beb22e35e0168003d05931''',
-
-  '''egc:election:3:d8799f582044868d8028d85581
-  bc2f9f0d8887fe50fbc937a0abc1afb5ca2e8f8dcdba
-  c01600ff:2:addr_
-  test1vr93qqyu30r5c7snd4wp8wu243st2xz8605yea78hgyg6uck
-  jakk5:118279899:156914063b8ea786ba55
-  764cc5c77b4b0ecf301789cd24a3ed9a2a0d3129ca36
-
-
-  ''',
+# happy_election
+'egc:election:3:d8799f5820c81cf66e6e2c118f96d1e9b064b693d6def8a451e8b030372dd69c172a90c9e800ff:2:addr_test1vr93qqyu30r5c7snd4wp8wu243st2xz8605yea78hgyg6uckjakk5:118296546:b23e0378ce52c791f9c449be3f1f44ab3a79be15adb309ccfb9751ab5f2f8392',
 
 ]
 
