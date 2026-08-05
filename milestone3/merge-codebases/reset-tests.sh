05f151400af1246580ace9e53e9a73e6ed043b0f
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-08-05 14:40:56 -0700

    reset-test.sh also kill pytest (somewhat broken)

--- milestone3/merge-codebases/reset-tests.sh
+++ milestone3/merge-codebases/reset-tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -x
+pytest_pid="$(ps aux | grep 'bin/pytest' | head -n1 | awk '{print $2}')"
+[[ -z $pytest_pid ]] || kill -9 $pytest_pid
 [[ $(docker ps | wc -l) -gt 1 ]] && (docker ps | grep 'egc-test' | awk '{print $1}' | xargs docker stop)
 [[ $(docker network ls | grep test | wc -l) -gt 0 ]] && (docker network ls| grep 'egc-test' | awk '{print $1}' | xargs docker network rm)
 rm -rf data/env*
