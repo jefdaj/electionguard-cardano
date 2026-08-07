b9c7d844c251a7a02029974db458199ced875d63
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-08-07 14:17:17 -0700

    fix reset-tests.sh egc-minimal network down

--- milestone3/merge-codebases/reset-tests.sh
+++ milestone3/merge-codebases/reset-tests.sh
@@ -3,6 +3,6 @@ set -x
 pytest_pid="$(ps aux | grep 'bin/pytest' | head -n1 | awk '{print $2}')"
 [[ -z $pytest_pid ]] || kill -9 $pytest_pid
 [[ $(docker ps | wc -l) -gt 1 ]] && (docker ps | grep 'egc-' | awk '{print $1}' | xargs docker stop)
-[[ $(docker network ls | grep test | wc -l) -gt 0 ]] && (docker network ls| grep 'egc-' | awk '{print $1}' | xargs docker network rm)
+[[ $(docker network ls | grep 'egc-' | wc -l) -gt 0 ]] && (docker network ls| grep 'egc-' | awk '{print $1}' | xargs docker network rm)
 rm -rf data/env*
 nix build .#dockerImage && docker load < result
