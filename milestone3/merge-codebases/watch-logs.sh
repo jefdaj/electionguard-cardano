92275911d70018a1706bb2f37554bc2033934a74
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-08-09 11:34:37 -0700

    fix watch-logs.sh

--- milestone3/merge-codebases/watch-logs.sh
+++ milestone3/merge-codebases/watch-logs.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 # Tails all the test logs.
 
-ptn='./data/env3/test*/data/*/*/test.log'
-multitail -Q 1 "$ptn"  --closeidle 60
+# TODO also include env1, env2
+
+base="./data/env3/test*/data/*/*/egc"
+multitail --closeidle 60 \
+  -Q 3 "$base/node.log" \
+  -Q 3 "$base/script.log"
