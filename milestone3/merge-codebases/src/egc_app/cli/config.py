0169d496657390aa75c65c6e41f3687eae9cee98
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-08-09 10:09:09 -0700

    egc config cmds cleanup

--- milestone3/merge-codebases/src/egc_app/cli/config.py
+++ milestone3/merge-codebases/src/egc_app/cli/config.py
@@ -9,33 +9,31 @@ def config() -> None:
     "View and manage the EGC node config."
 
 @config.command()
-@click.pass_context
-def role(ctx):
-    "Get current election role."
-    # The CLI calls the node API for the current config *every* time
-    # it's invoked, so now we can just pull role out of the context:
-    # TODO is this any different from a Client.config() call version?
-    role = ctx.find_root().default_map.get("node").get("role")
-    click.echo(role)
+def show():
+    """Print your current config as JSON.
+    Use jq to access fields if needed.
+    Examples:
 
-# only needed for root_ctx in the merge below:
-# @click.pass_context
-# def save(ctx, out_json):
+    \b
+        MY_PORT=$(egc config show | jq '.node.port' -r)
+        MY_PRIV_DIR="$(egc config show | jq '.node.private_dir' -r)"
+    """
+    cfg = asyncio.run(Client().config())
+    click.echo(json.dumps(cfg))
+
 @config.command()
 @click.argument("out_json", type=click.Path(dir_okay=False, writable=True))
 def save(out_json):
     "Write your current config to JSON."
-
-    # This works but seems counter-intuitive...
-    # If you call: egc --config <in-json> config save <out-json>
-    # You'll get <out-json> with overrides from <in-json>
-    # TODO would that ever be useful?
-    # root_ctx = ctx.find_root()
-    # override = dict(root_ctx.default_map or {})
-    # live = asyncio.run(Client().config())
-    # saved = deep_merge(live, override)
-
     cfg = asyncio.run(Client().config())
     with open(out_json, "w", encoding="utf-8") as f:
         json.dump(cfg, f, indent=2)
     click.echo(f"Saved config → {out_json}")
+
+# TODO remove in favor of config show | jq?
+@config.command()
+@click.pass_context
+def role(ctx):
+    "Print your current election role."
+    cfg = asyncio.run(Client().config())
+    click.echo(cfg['node']['role'])
