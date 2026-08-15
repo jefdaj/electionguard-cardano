30588c181d8efd3b415f8c72913c845004e0bf7a
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-08-15 09:19:14 -0700

    watch-logs loop multitail

--- milestone3/merge-codebases/watch-logs.sh
+++ milestone3/merge-codebases/watch-logs.sh
@@ -4,7 +4,11 @@
 # TODO also include env1, env2
 
 base="./data/env3/test*/data/*/*/egc"
-multitail --closeidle 60 \
-       "pytest.log" \
-  -Q 3 "$base/node.log" \
-  -Q 3 "$base/script.log"
+
+# loop because it crashes a lot
+while sleep 3; do
+  multitail --closeidle 60 \
+         "pytest.log" \
+    -Q 3 "$base/node.log" \
+    -Q 3 "$base/script.log"
+done
