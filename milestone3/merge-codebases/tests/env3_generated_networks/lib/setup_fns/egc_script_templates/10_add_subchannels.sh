e5c464fa8cb1ed8f0f7e14229ee0fdf8d97bc870
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-08-17 11:00:14 -0700

    improve egc script render, add subchannels in batches

--- milestone3/merge-codebases/tests/env3_generated_networks/lib/setup_fns/egc_script_templates/10_add_subchannels.sh
+++ milestone3/merge-codebases/tests/env3_generated_networks/lib/setup_fns/egc_script_templates/10_add_subchannels.sh
@@ -1,26 +1,42 @@
 {% extends "08_admin_post_batch.sh" %}
 {% block body %}
 {{ super() }}
-# Normally the admin would be manage their requests individually,
-# but counting files is easier to script for the tests.
-n_subs=$(cat private/n_requests.txt)
-while sleep 3; do
-  n_pngs=$(ls qrcodes/channel-*.png | wc -l)
-  [[ $n_subs == $n_pngs ]] && break
+# TODO set -euo pipefail overall?
+
+readonly max_per_tx=4
+declare -A processed
+n_channels_posted=0
+n_subs=$(< private/n_requests.txt)
+
+create_channels() {
+  # create subchannels (authorize those publishers to post)
+  # send 20 ada per subchannel for tx fees,
+  # along with 5 for collateral (automatic)
+  # finally, also advance to ceremony phase
+  local -a batch=("$@")
+  local -a cmd=(egc channel create --subchannel-ada 20)
+  for f in "${batch[@]}"; do
+    cmd+=(--request-load-png "$f")
+  done
+  (( n_channels_posted + ${#batch[@]} == n_subs )) && cmd+=(--done-onboarding)
+  "${cmd[@]}" || { echo "ERROR: command failed" >&2; exit 1; }
+  for f in "${batch[@]}"; do processed["$f"]=1; done
+  (( n_channels_posted += ${#batch[@]} ))
+}
+
+while (( n_channels_posted < n_subs )); do
+  sleep 5
+  batch=()
+  for path in qrcodes/channel-*.png; do
+    [[ -e "$path" ]] || continue
+    [[ -v processed["$path"] ]] && continue
+    batch+=("$path")
+    if (( ${#batch[@]} >= max_per_tx )); then
+      create_channels "${batch[@]}"
+      batch=()
+    fi
+  done
 done
 
-# load requests from prospective subchannel publishers
-set +x
-reqs=()
-for f in qrcodes/channel-*.png; do
-  reqs+=(--request-load-png "$f")
-done
-set -x
-
-# create subchannels (authorize those publishers to post)
-# send 20 ada per subchannel for tx fees,
-# along with 5 for collateral (automatic)
-# finally, also advance to ceremony phase
-egc channel create "${reqs[@]}" --subchannel-ada 20 --done-onboarding
 egc phase await --phase config_ceremony_round1
 {% endblock %}
--- milestone3/merge-codebases/tests/env3_generated_networks/lib/setup_fns/render_egc_scripts.py
+++ milestone3/merge-codebases/tests/env3_generated_networks/lib/setup_fns/render_egc_scripts.py
@@ -14,6 +14,16 @@ JINJA_ENV = Environment(
     trim_blocks = True,
     lstrip_blocks = True,
     keep_trailing_newline = False,
+
+    # change bracket -> square bracket comment delimiters,
+    # because brackets can be used in bash arrays
+    # TODO also change the rest to match?
+    # block_start_string='[%',
+    # block_end_string='%]',
+    # variable_start_string='[[',
+    # variable_end_string=']]',
+    comment_start_string='[#',
+    comment_end_string='#]',
 )
 
 def render_egc_scripts(
@@ -44,32 +54,39 @@ def render_egc_scripts(
     log_path     = cfg.log_path()
     scripts_path = tmpdir_path / 'egc_scripts'
 
-    with log_path.open('a') as log_handle:
+    # with log_path.open('a') as log_handle:
         # TODO rewrite with LOG
-        def log(msg):
-            log_handle.writelines([msg + '\n'])
-            log_handle.flush()
-        log('render_egc_scripts start')
+        # def log(msg):
+        #     log_handle.writelines([msg + '\n'])
+        #     log_handle.flush()
+    LOG.debug('render_egc_scripts start')
 
-        log(f'render_egc_scripts mkdir {scripts_path}')
-        scripts_path.mkdir(parents=True, exist_ok=True)
+    LOG.debug(f'render_egc_scripts mkdir {scripts_path}')
+    scripts_path.mkdir(parents=True, exist_ok=True)
 
-        # for (node_role, node_cfg) in cfg.nodes.items():
-        for (node_role, n_nodes) in node_counts.items():
-            template_name = template_names[node_role]
+    # for (node_role, node_cfg) in cfg.nodes.items():
+    for (node_role, n_nodes) in node_counts.items():
+        template_name = template_names[node_role]
+        LOG.debug(f'template_name: {template_name}')
+        try:
             template = JINJA_ENV.get_template(template_name)
-            for node_index in range(1, n_nodes+1):
-                node_name = 'admin' if node_role == 'admin' else f'{node_role}{node_index}'
-                out_path = scripts_path / f'{node_name}.sh'
-                out_text = template.render(
-                    node_name    = node_name,
-                    node_role    = node_role,
-                    node_index   = node_index,
-                    # template_name = template_name,
-                    debug_script = True,
-                )
-                log(f'render_egc_scripts write {out_path}')
-                out_path.write_text(out_text)
-                os.chmod(out_path, 0o755)
+        except Exception as e:
+            LOG.error(f'Error while loading template {template_name}: {e}')
+            raise
+        LOG.debug('loaded template')
+        for node_index in range(1, n_nodes+1):
+            node_name = 'admin' if node_role == 'admin' else f'{node_role}{node_index}'
+            out_path = scripts_path / f'{node_name}.sh'
+            LOG.debug(f'out_path: {out_path}')
+            out_text = template.render(
+                node_name    = node_name,
+                node_role    = node_role,
+                node_index   = node_index,
+                # template_name = template_name,
+                debug_script = True,
+            )
+            LOG.debug(f'render_egc_scripts write {out_path}')
+            out_path.write_text(out_text)
+            os.chmod(out_path, 0o755)
 
-        log('render_egc_scripts done')
+    LOG.debug('render_egc_scripts done')
