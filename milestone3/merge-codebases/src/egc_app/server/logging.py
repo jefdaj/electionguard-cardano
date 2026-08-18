34a9a24d43e97f3465340099bc4f0b13a343da32
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-08-18 12:21:44 -0700

    logging debug ogmios

--- milestone3/merge-codebases/src/egc_app/server/logging.py
+++ milestone3/merge-codebases/src/egc_app/server/logging.py
@@ -13,13 +13,13 @@ def setup_logging(log_file=None):
     logging.getLogger('aiohttp').setLevel(logging.WARNING)
 
     logging.getLogger('egc.core.ogmios').setLevel(logging.DEBUG)
-    logging.getLogger('egc.core.node').setLevel(logging.DEBUG)
-    logging.getLogger('egc.core.phase').setLevel(logging.DEBUG)
-    logging.getLogger('egc.core.publisher').setLevel(logging.DEBUG)
-    logging.getLogger('egc.core.records').setLevel(logging.DEBUG)
-    logging.getLogger('egc.core.subscriber').setLevel(logging.INFO)
-    logging.getLogger('egc.nodes.admin').setLevel(logging.DEBUG)
-    logging.getLogger('egc.nodes.ipfs').setLevel(logging.DEBUG)
+    # logging.getLogger('egc.core.node').setLevel(logging.DEBUG)
+    # logging.getLogger('egc.core.phase').setLevel(logging.DEBUG)
+    # logging.getLogger('egc.core.publisher').setLevel(logging.DEBUG)
+    # logging.getLogger('egc.core.records').setLevel(logging.DEBUG)
+    # logging.getLogger('egc.nodes.admin').setLevel(logging.DEBUG)
+    # logging.getLogger('egc.nodes.ipfs').setLevel(logging.DEBUG)
+    # logging.getLogger('egc.core.subscriber').setLevel(logging.DEBUG)
 
 
     logging.config.dictConfig({
