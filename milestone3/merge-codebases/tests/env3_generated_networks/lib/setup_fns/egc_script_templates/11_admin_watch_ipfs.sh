923bdc3493d43f54e2bfd8e27ce421244971a026
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-08-18 16:59:15 -0700

    fix: n_subs -> N_SUBS

--- milestone3/merge-codebases/tests/env3_generated_networks/lib/setup_fns/egc_script_templates/11_admin_watch_ipfs.sh
+++ milestone3/merge-codebases/tests/env3_generated_networks/lib/setup_fns/egc_script_templates/11_admin_watch_ipfs.sh
@@ -6,7 +6,7 @@ n_old=0
 while sleep 10; do
   n_new=$(egc ipfs show | jq '.channel_nodes' | grep "peer_id" | wc -l)
   [[ $n_new > $n_old  ]] && egc ipfs show | jq
-  [[ $n_new > $n_subs ]] && break
+  [[ $n_new > $N_SUBS ]] && break
   n_old=$n_new
 done
 {% endblock %}
