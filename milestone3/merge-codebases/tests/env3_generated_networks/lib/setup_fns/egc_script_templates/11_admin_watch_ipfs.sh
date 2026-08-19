bb846e276249fadff712f40f6c10a8c7e3ae12b0
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-08-18 18:01:08 -0700

    fix sub ipfs id show script

--- milestone3/merge-codebases/tests/env3_generated_networks/lib/setup_fns/egc_script_templates/11_admin_watch_ipfs.sh
+++ milestone3/merge-codebases/tests/env3_generated_networks/lib/setup_fns/egc_script_templates/11_admin_watch_ipfs.sh
@@ -1,12 +1,10 @@
 {% extends "10_add_subchannels.sh" %}
 {% block body %}
 {{ super() }}
-# Wait for subchannel publishers' ipfs peer_ids to appear on chain
-n_old=0
+# Wait for all subchannel publishers' ipfs peer_ids to appear on chain
 while sleep 10; do
-  n_new=$(egc ipfs show | jq '.channel_nodes' | grep "peer_id" | wc -l)
-  [[ $n_new > $n_old  ]] && egc ipfs show | jq
-  [[ $n_new > $N_SUBS ]] && break
-  n_old=$n_new
+  n_ids=$(egc ipfs show | jq '.channel_nodes' | grep "peer_id" | wc -l)
+  [[ $n_ids > $N_SUBS ]] && break
 done
+egc ipfs show | jq
 {% endblock %}
