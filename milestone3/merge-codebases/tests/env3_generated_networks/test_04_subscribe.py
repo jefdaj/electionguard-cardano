71ec99a88af4b99851f98280567d83eb0db0bcfe
jefdaj <1198065+jefdaj@users.noreply.github.com>
2026-09-08 12:27:02 -0700

    test_04_subscribe pass if timed out when no election started

--- milestone3/merge-codebases/tests/env3_generated_networks/test_04_subscribe.py
+++ milestone3/merge-codebases/tests/env3_generated_networks/test_04_subscribe.py
@@ -26,13 +26,10 @@ def cfg_subscribe_txt(draw):
 
 def assert_election_events(cfg):
     assert_script_logs_match(cfg, '.*', [
-
-        '^[0-9]{9,}\\s.*funder authorized admin',
-
-        # some of the test qr_strs point to elections that never finished,
-        # so timing out is the expected/correct behavior
+        # some of the test qr_strs point to elections that never finished (or never started!),
+        # in which case timing out is the correct behavior
+        '^[0-9]{9,}\\s.*(funder authorized admin|subscriber timed out)',
         '^[0-9]{9,}\\s.*(ended election|subscriber timed out)',
-
    ])
 
 
